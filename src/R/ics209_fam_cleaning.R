1768f429213cc0fc6baf4f88dc0452cab8211c2f
‘NateMietk’ <natemietkiewicz@gmail.com>
2017-10-21 12:09:25 -0600

    updates

--- src/R/ics209_fam_cleaning.R
+++ src/R/ics209_fam_cleaning.R
@@ -1,18 +1,24 @@
+source("src/R/helper_functions.R")
 
-x <- c("data.table", "tidyverse", "tidyverse", "magrittr", "sf", 
-       "assertthat", "purrr", "httr", "rvest", "lubridate")
+x <- c("data.table", "tidyverse", "tidyverse", "magrittr", "sf",
+       "assertthat", "purrr", "httr", "rvest", "lubridate", "parallel")
 lapply(x, library, character.only = TRUE, verbose = FALSE)
 
 ## Download and process State data
 # Creat directories for state data
-raw_prefix <- file.path("data", "raw")
-us_prefix <- file.path(raw_prefix, "cb_2016_us_state_20m")
-ics_prefix <- file.path("data", "ics209")
+prefix <- ifelse(Sys.getenv("LOGNAME") == "NateM", file.path("data"),
+                 ifelse(Sys.getenv("LOGNAME") == "nami1114", file.path("data"),
+                        file.path("../data")))
+raw_prefix <- file.path(prefix, "raw")
+us_prefix <- file.path(raw_prefix, "cb_2016_us_state_5m")
+ics_prefix <- file.path(prefix, "ics209")
 
 # Check if directory exists for all variable aggregate outputs, if not then create
-var_dir <- list(raw_prefix, us_prefix, ics_prefix)
+var_dir <- list(prefix, raw_prefix, us_prefix, ics_prefix)
 
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
+
+ncores <- detectCores()
 
 us_shp <- file.path(us_prefix, "cb_2016_us_state_5m.shp")
 if (!file.exists(us_shp)) {
@@ -26,7 +32,7 @@ if (!file.exists(us_shp)) {
 
 usa_shp <- st_read(dsn = us_prefix,
                    layer = "cb_2016_us_state_5m", quiet= TRUE) %>%
-  st_transform("+proj=longlat +datum=WGS84") %>%  # e.g. US National Atlas Equal Area
+  st_par(., st_transform, n_cores = ncores, crs = "+proj=longlat +datum=WGS84") %>%  # e.g. US National Atlas Equal Area
   filter(!(NAME %in% c("Alaska", "Hawaii", "Puerto Rico",
                        "Commonwealth of the Northern Mariana Islands", "United States Virgin Islands",
                        "American Samoa", "Guam"))) %>%
@@ -36,14 +42,14 @@ usa_shp <- st_read(dsn = us_prefix,
 source("src/R/helper_functions.R")
 
 fam_rep <- fread("data/ics209/input_tbls/famweb/ics209_2001_2013_wfonlyv3.csv") %>%
-  mutate_all(funs(replace(., is.na(.), 0))) 
+  mutate_all(funs(replace(., is.na(.), 0)))
 
-est_lookup <- fread("data/ics209/input_tbls/famweb/clean_estimated_costs.csv") 
+est_lookup <- fread("data/ics209/input_tbls/famweb/clean_estimated_costs.csv")
 latlong <- fread("data/ics209/input_tbls/latlong/ics209Incidents-cleaned_ll.csv")
 
-names(fam_rep) %<>% tolower 
+names(fam_rep) %<>% tolower
 
-fam <- fam_rep %>% 
+fam <- fam_rep %>%
   filter(!(un_ustate %in% c("AK", "HI", "PR"))) %>%
   filter(type_inc != "RX") %>%
   mutate(long = -longitude,
@@ -66,7 +72,7 @@ fam <- fam_rep %>%
          area_km2 = area_ha*0.01,
          est_final_costs = dollarToNumber_vectorised(clean_estimated_costs(incident_unique_id, est_final_costs)),
          costs_to_date_c = dollarToNumber_vectorised(clean_costs_to_date(incident_unique_id, costs_to_date)),
-         cause_binary = ifelse(cause == "H", "2", 
+         cause_binary = ifelse(cause == "H", "2",
                                ifelse(cause =="L", "1", "0")),
          confi = "H") %>%
   left_join(est_lookup, by = c("incident_unique_id", "est_final_costs")) %>%
@@ -98,7 +104,7 @@ fam_clean <- fam %>%
             tot.aerial = sum(imsr_num_aerial),
             max.agency.support = max(imsr_num_agencies),
             cause = max(cause_binary),
-            cause = ifelse(cause == "2", "Human", 
+            cause = ifelse(cause == "2", "Human",
                            ifelse(cause =="1", "Lightning", "Unk")),
             confi = last(confi)) %>%
   left_join(., latlong, by = "incident_unique_id") %>%
