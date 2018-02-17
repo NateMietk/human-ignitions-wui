c2e29ddf32b3fc97eb3ac95fcd3227c06a0095ba
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-02-16 19:22:50 -0700

    minor changes

--- src/R/figures/figure1.R
+++ src/R/figures/figure1.R
@@ -1,11 +1,13 @@
 # Overall totals by SIZE AND CLASS AND CAUSE
 totals_by_cause_class_sizes <- wui_209 %>% 
+  filter(class == "WUI") %>%
   mutate(cause = if_else(cause == "Unk", "Human", cause)) %>%
   group_by(syear, class, cause) %>%
   summarise(size = sum(area_km2)) %>%
   as.data.frame(.) 
 
 ics_sums <-   wui_209 %>%
+  filter(class == "WUI") %>%
   mutate(cause = if_else(cause == "Unk", "Human", cause)) %>%
   group_by(syear, cause, class) %>%
   summarise(costs = sum(costs)) %>%
@@ -20,7 +22,7 @@ ics_sums_p <- ics_sums %>%
   geom_point(aes(y = costs/100000000, color = cause), size = 2) +
   geom_line(aes(y = costs/100000000, color = cause),  size = 0.5, alpha = 0.25) +
   geom_smooth(aes(y = costs/100000000, color = cause), method="glm", method.args = list(family = "poisson"),
-              se= FALSE, size = 0.75) +
+              se= FALSE, size = 0.75) +  
   scale_color_manual(values = c("#D62728", "#1F77B4")) +
   scale_y_continuous(breaks = pretty(ics_sums$costs/100000000, n = 5)) +
   xlab("Year") + ylab("Estimated fire Suppression Cost \n(in hundreds of millions of dollars; $)") +
@@ -46,7 +48,7 @@ ics_sums_s <- ics_sums %>%
         strip.text = element_text(size = 10, face = "bold"),
         legend.position = "none")
 
-grid.arrange(ics_sums_p, ics_sums_s, ncol =1)
+grid.arrange(ics_sums_p, ics_sums_s, ncol =2)
 g <- arrangeGrob(ics_sums_p, ics_sums_s, ncol =2)
-ggsave(file = "results/Ignition_Costs_0213_HL.pdf", g, width = 8, height = 4, dpi=1200, scale = 2.5, units = "cm")
+ggsave(file = "figs/draft/main_text/figure1_wui_impact.pdf", g, width = 8, height = 4, dpi=1200, scale = 3, units = "cm")
 
