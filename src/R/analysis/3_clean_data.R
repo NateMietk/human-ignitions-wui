f2046954fe58589ca6102086e0adf3c73ff9d8fa
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-02-16 19:22:01 -0700

    the transformations now use the usa_shp crs

--- src/R/analysis/3_clean_data.R
+++ src/R/analysis/3_clean_data.R
@@ -1,10 +1,9 @@
 
 usa_shp <- st_read(dsn = us_prefix,
                    layer = "cb_2016_us_state_20m", quiet= TRUE) %>%
-  st_transform("+init=epsg:2163") %>%  # e.g. US National Atlas Equal Area
   filter(!(NAME %in% c("Alaska", "Hawaii", "Puerto Rico"))) %>%
+  st_transform("+init=epsg:2163") %>%  # e.g. US National Atlas Equal Area
   dplyr::select(STUSPS)
-
 names(usa_shp) %<>% tolower
 
 states <- as(usa_shp, "Spatial")
@@ -15,7 +14,7 @@ names(st_df) <- tolower(names(st_df))
 
 # Import the Level 3 Ecoregions
 ecoreg <- st_read(dsn = ecoregion_prefix, layer = "us_eco_l3", quiet= TRUE) %>%
-  st_transform("+init=epsg:2163") %>%  # e.g. US National Atlas Equal Area
+  st_transform(st_crs(usa_shp)) %>%  # e.g. US National Atlas Equal Area
   st_simplify(., preserveTopology = TRUE, dTolerance = 1000) %>%
   dplyr::select(US_L3CODE, US_L3NAME, NA_L2CODE, NA_L2NAME, NA_L1CODE, NA_L1NAME) %>%
   st_intersection(., usa_shp) %>%
@@ -36,7 +35,7 @@ names(ecoreg) %<>% tolower
 
 ecoreg_swse <- ecoreg %>%
   group_by(regions) %>%
-  summarize()
+  summarize() 
 
 ecoregs <- as(ecoreg_swse, "Spatial")
 ecoregs$id <- row.names(ecoregs)
@@ -47,7 +46,7 @@ names(ec_df) <- tolower(names(ec_df))
 p1 <- ggplot() +
   geom_polygon(data = ec_df, 
                aes(x = long,y = lat,group=group), 
-               color='black', fill = "gray99", size = .25)+
+               color='black', fill = "gray99", size = .1)+
   coord_equal() +
   theme_nothing(legend = TRUE) +
   theme(plot.title = element_text(hjust = 0, size = 12),
@@ -55,11 +54,12 @@ p1 <- ggplot() +
         strip.text.x = element_text(size = 12, face = "bold"),
         strip.text.y = element_text(size = 12),
         legend.key = element_rect(fill = "white"))
+ggsave(file = "figs/draft/main_text/regions.eps", p1, width = 1.75, height = 1, dpi=1200) #saves g
 
 # Create Fishnets ---------------------------------------------------------
 # 50k Fishnet
 fishnet_50k <- st_make_grid(usa_shp, cellsize = 50000, what = 'polygons') %>%
-  st_sf('geometry' = ., data.frame('fish50' = 1:length(.))) %>%
+  st_sf('geometry' = ., data.frame('fishid50k' = 1:length(.))) %>%
   st_intersection(., st_union(usa_shp))
 
 # 25k Fishnet
@@ -74,7 +74,7 @@ fs25_df <- data.frame(fs25_df)
 
 # 10k Fishnet
 fishnet_10k <- st_make_grid(usa_shp, cellsize = 10000, what = 'polygons') %>%
-  st_sf('geometry' = ., data.frame('fish10' = 1:length(.))) %>%
+  st_sf('geometry' = ., data.frame('fishid10k' = 1:length(.))) %>%
   st_intersection(., st_union(usa_shp))
 
 # Intersects the region
@@ -86,12 +86,12 @@ bounds <- st_intersection(usa_shp, ecoreg) %>%
 #Import the Wildland-Urban Interface and process
 if(!file.exists(file.path(anthro_out, "wui_bounds.gpkg"))) {
   wui <- st_read(dsn = file.path(wui_prefix, "us_wui_2010.gdb"),
-               layer = "us_wui_2010") %>%
-      st_simplify(., preserveTopology = TRUE, dTolerance = 0.001) %>%
-      mutate(Class = classify_wui(WUICLASS10)) %>%
-      filter(Class %in% c("Urban" ,"WUI", "VLD", "Wildlands")) %>%
-      st_transform("+init=epsg:2163") %>%
-      st_make_valid() 
+             layer = "us_wui_2010") %>%
+    st_simplify(., preserveTopology = TRUE, dTolerance = 0.001) %>%
+    mutate(Class = classify_wui(WUICLASS10)) %>%
+    filter(Class %in% c("Urban" ,"WUI", "VLD", "Wildlands")) %>%
+    st_transform(st_crs(usa_shp)) %>%  # e.g. US National Atlas Equal Area
+    st_make_valid() 
   wui_bounds <- bounds %>%
     st_make_valid() %>%
     st_intersection(., wui) %>%
@@ -121,7 +121,8 @@ if(!file.exists(file.path(fpa_out, "fpa_conus.gpkg"))) {
            DISCOVERY_MONTH = month(DISCOVERY_DATE),
            DISCOVERY_YEAR = FIRE_YEAR)  %>%
     st_make_valid() %>%
-    st_transform(fpa_fire, "+init=epsg:2163")
+    st_transform(st_crs(usa_shp)) %>%
+    st_intersection(., st_union(usa_shp))
   names(fpa_fire) %<>% tolower
   
   st_write(fpa_fire, file.path(fpa_out, "fpa_conus.gpkg"),
@@ -129,7 +130,8 @@ if(!file.exists(file.path(fpa_out, "fpa_conus.gpkg"))) {
            update=TRUE)
 } else {
   fpa_fire <- st_read(dsn = file.path(fpa_out, "fpa_conus.gpkg")) %>%
-    st_transform(fpa_fire, "+init=epsg:2163")
+    st_transform(st_crs(usa_shp)) %>%
+    st_intersection(., st_union(usa_shp))
 }
 
 # Spatially join the fpa database to the WUI
@@ -151,21 +153,21 @@ if(!file.exists(file.path(fpa_out, "fpa_wui_conus.gpkg"))) {
 if (!file.exists(file.path(mtbs_out, "mtbs_conus.gpkg"))) {
   mtbs_fire <- st_read(dsn = mtbs_prefix,
                      layer = "mtbs_perims_1984-2015_DD_20170815", quiet= TRUE) %>%
-  st_transform("+init=epsg:2163") %>%
-  filter(Year >= 1992) %>%
-  mutate(MTBS_ID = Fire_ID,
-         MTBS_FIRE_NAME = Fire_Name,
-         MTBS_DISCOVERY_YEAR = Year,
-         MTBS_DISCOVERY_DAY = day(ig_date),
-         MTBS_DISCOVERY_MONTH = month(ig_date),
-         MTBS_DISCOVERY_DOY = yday(ig_date),
-         RADIUS = "NA") %>%
-  dplyr::select(MTBS_ID, MTBS_FIRE_NAME, MTBS_DISCOVERY_DOY, MTBS_DISCOVERY_DAY, MTBS_DISCOVERY_MONTH, MTBS_DISCOVERY_YEAR) %>%
-  merge(., as.data.frame(fpa_fire), by = c("MTBS_ID", "MTBS_FIRE_NAME"), all = FALSE) %>%
-  dplyr::select(FPA_ID, LATITUDE, LONGITUDE, ICS_209_INCIDENT_NUMBER, ICS_209_NAME, MTBS_ID, MTBS_FIRE_NAME, FIRE_SIZE, FIRE_SIZE_m2, FIRE_SIZE_ha, FIRE_SIZE_km2,
-                MTBS_DISCOVERY_YEAR, DISCOVERY_YEAR, MTBS_DISCOVERY_DOY, DISCOVERY_DOY, MTBS_DISCOVERY_MONTH, DISCOVERY_MONTH,
-                MTBS_DISCOVERY_DAY, DISCOVERY_DAY, STATE, STAT_CAUSE_DESCR, IGNITION, RADIUS)  %>%
-  st_make_valid()
+    st_transform(st_crs(usa_shp)) %>%
+    filter(Year >= 1992) %>%
+    mutate(MTBS_ID = Fire_ID,
+           MTBS_FIRE_NAME = Fire_Name,
+           MTBS_DISCOVERY_YEAR = Year,
+           MTBS_DISCOVERY_DAY = day(ig_date),
+           MTBS_DISCOVERY_MONTH = month(ig_date),
+           MTBS_DISCOVERY_DOY = yday(ig_date),
+           RADIUS = "NA") %>%
+    dplyr::select(MTBS_ID, MTBS_FIRE_NAME, MTBS_DISCOVERY_DOY, MTBS_DISCOVERY_DAY, MTBS_DISCOVERY_MONTH, MTBS_DISCOVERY_YEAR) %>%
+    merge(., as.data.frame(fpa_fire), by = c("MTBS_ID", "MTBS_FIRE_NAME"), all = FALSE) %>%
+    dplyr::select(FPA_ID, LATITUDE, LONGITUDE, ICS_209_INCIDENT_NUMBER, ICS_209_NAME, MTBS_ID, MTBS_FIRE_NAME, FIRE_SIZE, FIRE_SIZE_m2, FIRE_SIZE_ha, FIRE_SIZE_km2,
+                  MTBS_DISCOVERY_YEAR, DISCOVERY_YEAR, MTBS_DISCOVERY_DOY, DISCOVERY_DOY, MTBS_DISCOVERY_MONTH, DISCOVERY_MONTH,
+                  MTBS_DISCOVERY_DAY, DISCOVERY_DAY, STATE, STAT_CAUSE_DESCR, IGNITION, RADIUS)  %>%
+    st_make_valid()
   names(mtbs_fire) %<>% tolower
   
   st_write(mtbs_fire, file.path(mtbs_out, "mtbs_conus.gpkg"),
