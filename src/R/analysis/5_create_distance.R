f227f9bced703a5313f43e315fa611a1ca45c5d7
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-10 19:32:58 +0000

    single core distance for looped

--- src/R/analysis/5_create_distance.R
+++ src/R/analysis/5_create_distance.R
@@ -1,5 +1,4 @@
 
-
 # Calculate the distance of each fire point to Urban boundary.
 if (!file.exists(file.path(wui_out, "high_den_urban_1990.gpkg"))) {
   urban_1990 <- wui %>%
@@ -8,7 +7,7 @@ if (!file.exists(file.path(wui_out, "high_den_urban_1990.gpkg"))) {
     st_make_valid()  %>%
     st_transform(crs = proj_ed) %>%
     st_union()
-
+  
   st_write(urban_1990,
            file.path(wui_out, "high_den_urban_1990.gpkg"),
            driver = "GPKG")
@@ -27,7 +26,7 @@ if (!file.exists(file.path(wui_out, "high_den_urban_2000.gpkg"))) {
     st_make_valid()  %>%
     st_transform(crs = proj_ed) %>%
     st_union()
-
+  
   st_write(urban_2000,
            file.path(wui_out, "high_den_urban_2000.gpkg"),
            driver = "GPKG")
@@ -46,7 +45,7 @@ if (!file.exists(file.path(wui_out, "high_den_urban_2010.gpkg"))) {
     st_make_valid()  %>%
     st_transform(crs = proj_ed) %>%
     st_union()
-
+  
   st_write(urban_2010,
            file.path(wui_out, "high_den_urban_2010.gpkg"),
            driver = "GPKG")
@@ -75,53 +74,79 @@ fpa_fire_ed <- fpa_wui %>%
 fpa_fire_ed$STATE <- droplevels(fpa_fire_ed$STATE)
 fpa_fire_ed$FPA_ID <- droplevels(fpa_fire_ed$FPA_ID)
 
-fpa_fire_ed <- fpa_fire_ed %>%
-  split(.$decade)
+fpa_slim <- fpa_fire_ed %>%
+  slice(1:25)
+state <- unique(fpa_slim$STATE)
+decades <- unique(fpa_slim$decade)
 
-urban_distance <- lapply(fpa_fire_ed,
-                         function (input_list) {
-                           require(tidyverse)
-                           require(magrittr)
-                           require(lubridate)
-                           require(lubridate)
-                           require(sf)
-                           library(nabor)
-
-                           sub_grid <-
-                             dplyr:::bind_cols(input_list)
-                           unique_ids <-
-                             unique(sub_grid$FPA_ID)
-                           decades <-
-                             unique(sub_grid$decade)[1]
-
-                           get_polygons <-
-                             function(decades) {
-                               if (decades == '1990') {
-                                 polygons <- urban_1990
-                               }  else if (decades == '2000') {
-                                 polygons <- urban_2000
-                               } else  if (decades == '2010') {
-                                 polygons <- urban_2010
-                               }
-                               polygons
-                             }
-
-                           polygons <- get_polygons(decades)
-                           centroids <- polygons %>%
-                             st_centroid(.)
-
-                           print(paste0('Working on ', decades))
-
-                           got_distance <- lapply(
-                             unique_ids,
-                             FUN = get_distance,
-                             points = sub_grid,
-                             polygons = polygons,
-                             centroids = centroids
-                           )
-                           print(paste0('Finishing ', decades))
-                           got_distance <-
-                             do.call(rbind, got_distance)
-
-                           return(got_distance)
-                         })
+for (i in decades) {
+  decade_df <- subset(fpa_slim, fpa_slim$decade == i)
+  
+  polygons <- get_polygons(i)
+  
+  centroids <- polygons %>%
+    st_centroid(.)
+  
+  distance_to_fire_full <- list()
+  
+  for (j in state) {
+    
+    state_df <- subset(decade_df, decade_df$STATE == j)
+    state_df$STATE <- droplevels(state_df$STATE)
+    
+    unique_ids <- unique(state_df$FPA_ID)
+    
+    total_ids <- length(unique_ids)
+    
+    distance_to_fire <- list()
+    
+    print(paste0('Working on ', j , ' ', i))
+    pb <- txtProgressBar(min = 0, max = total_ids, style = 3)
+    
+    for (h in 1:length(unique_ids)) {
+      fpa_df <- subset(state_df, state_df$FPA_ID == unique_ids[h])
+      
+      closest_centroids <-
+        nabor::knn(coordinates(as(centroids, 'Spatial')),
+                   coordinates(as(fpa_df, 'Spatial')), k = 5) %>%
+        bind_cols() %>%
+        mutate(
+          poly_ids = nn.idx,
+          knn_distance = nn.dists,
+          FPA_ID = as.data.frame(fpa_df)$FPA_ID
+        ) %>%
+        dplyr::select(-nn.idx,-nn.dists) %>%
+        left_join(., polygons, by = 'poly_ids') %>%
+        sf::st_sf()
+      
+      distance_to_fire[[h]] <- fpa_df %>%
+        dplyr::select(-FPA_ID) %>%
+        mutate(
+          distance_to_urban = min(st_distance(
+            st_geometry(closest_centroids),
+            st_geometry(.),
+            by_element = TRUE
+          )),
+          FPA_ID = data.frame(fpa_df)$FPA_ID
+        )
+      setTxtProgressBar(pb, h)
+      
+      }
+    close(pb)
+    distance_to_fire_full[[j]] <- do.call(rbind, distance_to_fire)
+    distance_to_fire_statedf <- do.call(rbind, distance_to_fire)
+    
+    write_rds(distance_to_fire_statedf, file.path(distance_state_out, paste0('distance_fpa_', i, '_', j, '.rds')))
+    system('aws s3 sync data/anthro/wui/distance_from_urban s3://earthlab-modeling-human-ignitions/anthro/wui/distance_from_urban')
+    
+  }
+  
+  distance_to_fire_full <- do.call(rbind, distance_to_fire_full) # Convert to data frame format
+  
+  # save the final cleaned climate extractions joined with the fpa-fod database
+  summary_name <- file.path(distance_out, paste0('distance_fpa_', i, '.rds'))
+  write_rds(distance_to_fire_full, summary_name)
+  
+  # push to S3
+  system('aws s3 sync data/anthro/wui/distance_from_urban s3://earthlab-modeling-human-ignitions/anthro/wui/distance_from_urban')
+} 
