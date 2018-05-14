ab8f12c19e8a33ce272329085945d056930336fa
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-14 17:16:24 -0600

    put KNN within the decade loop and tried a small foreach loop.

--- src/R/analysis/5_create_distance.R
+++ src/R/analysis/5_create_distance.R
@@ -57,7 +57,7 @@ if (!file.exists(file.path(wui_out, "high_den_urban_2010.gpkg"))) {
     mutate(poly_ids = row_number())
 }
 
-fpa_fire_ed <- fpa_wui %>%
+fpa_fire_ed <- fpa_fire %>%
   dplyr::select(FPA_ID, DISCOVERY_YEAR, STATE) %>%
   mutate(
     DISCOVERY_YEAR = as.numeric(DISCOVERY_YEAR),
@@ -73,75 +73,92 @@ fpa_fire_ed <- fpa_wui %>%
 
 fpa_fire_ed$STATE <- droplevels(fpa_fire_ed$STATE)
 fpa_fire_ed$FPA_ID <- droplevels(fpa_fire_ed$FPA_ID)
+decades <- unique(fpa_fire_ed$decade)
 
 for (i in decades) {
-  decade_df <- subset(fpa_fire_ed, fpa_fire_ed$decade == i)
+  if (!exists(file.path(distance_out, paste0('distance_fpa_', i, '.rds')))) {
+    decade_df <- subset(fpa_fire_ed, fpa_fire_ed$decade == i)
 
-  polygons <- get_polygons(i)
+    polygons <- get_polygons(i)
 
-  centroids <- polygons %>%
-    st_centroid(.)
+    centroids <- polygons %>%
+      st_centroid(.)
 
-  distance_to_fire_full <- list()
+    nearest_neighbors <- as.tibble(knn(st_coordinates(centroids),
+                                       st_coordinates(decade_df),
+                                       k = 4)$nn.idx) %>%
+      mutate(FPA_ID = as.data.frame(decade_df)$FPA_ID) %>%
+      gather(var, poly_ids,-FPA_ID) %>%
+      dplyr::select(-var)
 
-  for (j in state) {
+    nearest_neighbors <- nearest_neighbors %>%
+      left_join(., polygons, by = 'poly_ids') %>%
+      st_sf()
+    distance_to_fire_full <- list()
 
-    state_df <- subset(decade_df, decade_df$STATE == j)
-    state_df$STATE <- droplevels(state_df$STATE)
+    state <- unique(decade_df$STATE)
 
-    unique_ids <- unique(state_df$FPA_ID)
+    for (j in state) {
+      state_df <- subset(decade_df, decade_df$STATE == j)
+      state_df$STATE <- droplevels(state_df$STATE)
+      distance_to_fire <- list()
 
-    total_ids <- length(unique_ids)
+      unique_ids <- unique(state_df$FPA_ID)
+      total_ids <- length(unique_ids)
 
-    distance_to_fire <- list()
+      print(paste0('Working on ', j , ' ', i))
 
-    print(paste0('Working on ', j , ' ', i))
-    pb <- txtProgressBar(min = 0, max = total_ids, style = 3)
+      cl <- makeCluster(2)
+      registerDoParallel(cl)
 
-    for (h in 1:length(unique_ids)) {
-      fpa_df <- subset(state_df, state_df$FPA_ID == unique_ids[h])
+      distance_to_fire <- foreach (h = 1:length(unique_ids), .combine = 'rbind') %dopar% {
+        fpa_ids <- unique_ids[h]
+        fpa_df <- subset(state_df, state_df$FPA_ID == fpa_ids)
 
-      closest_centroids <-
-        nabor::knn(coordinates(as(centroids, 'Spatial')),
-                   coordinates(as(fpa_df, 'Spatial')), k = 5) %>%
-        bind_cols() %>%
-        mutate(
-          poly_ids = nn.idx,
-          knn_distance = nn.dists,
-          FPA_ID = as.data.frame(fpa_df)$FPA_ID
-        ) %>%
-        dplyr::select(-nn.idx,-nn.dists) %>%
-        left_join(., polygons, by = 'poly_ids') %>%
-        sf::st_sf()
+        closest_centroids <-
+          subset(nearest_neighbors,
+                 nearest_neighbors$FPA_ID == fpa_ids)
 
-      distance_to_fire[[h]] <- fpa_df %>%
-        dplyr::select(-FPA_ID) %>%
-        mutate(
-          distance_to_urban = min(st_distance(
-            st_geometry(closest_centroids),
-            st_geometry(.),
-            by_element = TRUE
-          )),
-          FPA_ID = data.frame(fpa_df)$FPA_ID
-        )
-      setTxtProgressBar(pb, h)
-
+        distance_to_fire <- fpa_df %>%
+          dplyr::select(-FPA_ID) %>%
+          mutate(
+            distance_to_urban = min(
+              st_distance(
+                st_geometry(closest_centroids),
+                st_geometry(.),
+                by_element = TRUE
+              )
+            ),
+            FPA_ID = data.frame(fpa_df)$FPA_ID
+          )
       }
-    close(pb)
-    distance_to_fire_full[[j]] <- do.call(rbind, distance_to_fire)
-    distance_to_fire_statedf <- do.call(rbind, distance_to_fire)
+      stopCluster(cl)
 
-    write_rds(distance_to_fire_statedf, file.path(distance_state_out, paste0('distance_fpa_', i, '_', j, '.rds')))
-    system('aws s3 sync data/anthro/wui/distance_from_urban s3://earthlab-modeling-human-ignitions/anthro/wui/distance_from_urban')
+      distance_to_fire_full[[j]] <- do.call(rbind, distance_to_fire)
+      distance_to_fire_statedf <- do.call(rbind, distance_to_fire)
 
+      write_rds(distance_to_fire_statedf,
+                file.path(
+                  distance_state_out,
+                  paste0('distance_fpa_', i, '_', j, '.rds')
+                ))
+      system(
+        'aws s3 sync data/anthro/wui/distance_from_urban s3://earthlab-modeling-human-ignitions/anthro/wui/distance_from_urban'
+      )
+
+    }
+
+    distance_to_fire_full <-
+      do.call(rbind, distance_to_fire_full) # Convert to data frame format
+
+    # save the final cleaned climate extractions joined with the fpa-fod database
+    summary_name <-
+      file.path(distance_out, paste0('distance_fpa_', i, '.rds'))
+    write_rds(distance_to_fire_full, summary_name)
+
+    # push to S3
+    system(
+      'aws s3 sync data/anthro/wui/distance_from_urban s3://earthlab-modeling-human-ignitions/anthro/wui/distance_from_urban'
+    )
   }
-
-  distance_to_fire_full <- do.call(rbind, distance_to_fire_full) # Convert to data frame format
-
-  # save the final cleaned climate extractions joined with the fpa-fod database
-  summary_name <- file.path(distance_out, paste0('distance_fpa_', i, '.rds'))
-  write_rds(distance_to_fire_full, summary_name)
-
-  # push to S3
-  system('aws s3 sync data/anthro/wui/distance_from_urban s3://earthlab-modeling-human-ignitions/anthro/wui/distance_from_urban')
 }
