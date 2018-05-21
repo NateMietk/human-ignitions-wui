56b7d700d1ee9ad1888d04dd9ad4aee19ceb58bd
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-21 13:58:28 -0600

    updates

--- src/R/analysis/1_create_folders.R
+++ src/R/analysis/1_create_folders.R
@@ -23,19 +23,26 @@ fpa_prefix <- file.path(raw_prefix, "fpa-fod")
 mtbs_prefix <- file.path(raw_prefix, "mtbs_fod_perimeter_data")
 
 # Cleaned data output folders
-anthro_out <- file.path(prefix, "anthro")
-fire_crt <- file.path(prefix, "fire")
-nifc_crt <- file.path(fire_crt, "nifc")
-
 bounds_crt <- file.path(prefix, "bounds")
 ecoreg_crt <- file.path(bounds_crt, "ecoregions")
 swse_crt <- file.path(ecoreg_crt, "swse")
 ecoregion_out <- file.path(ecoreg_crt, "us_eco_l3")
 
-fpa_out <- file.path(fire_crt, "fpa-fod")
-mtbs_out <- file.path(fire_crt, "mtbs_fod_perimeter_data")
+anthro_out <- file.path(prefix, "anthro")
 wui_out <- file.path(anthro_out, "wui")
 distance_out <- file.path(wui_out, "distance_from_urban")
+ztrax_out <- file.path(anthro_out, 'ztrax')
+bui_out <- file.path(ztrax_out, 'built_up_intensity')
+bui_out <- file.path(ztrax_out, 'BUI')
+bu_out <- file.path(ztrax_out, 'building_counts')
+bu_out <- file.path(ztrax_out, 'building_counts_all')
+
+fire_crt <- file.path(prefix, "fire")
+nifc_crt <- file.path(fire_crt, "nifc")
+fpa_out <- file.path(fire_crt, "fpa-fod")
+mtbs_out <- file.path(fire_crt, "mtbs_fod_perimeter_data")
+fire_pnt <- file.path(fpa_out, 'points')
+fire_poly <- file.path(fpa_out, 'perimeters')
 
 ics_out <- file.path(fire_crt, "ics_209")
 ics_outtbls <- file.path(ics_out, "output_tbls")
@@ -46,12 +53,6 @@ ics_spatial <- file.path(ics_out, "spatial")
 
 fishnet_path <- file.path(bounds_crt, "fishnet")
 
-ztrax_out <- file.path(anthro_out, 'ztrax')
-bui_out <- file.path(ztrax_out, 'built_up_intensity')
-bui_out <- file.path(ztrax_out, 'BUI')
-bu_out <- file.path(ztrax_out, 'building_counts')
-bu_out <- file.path(ztrax_out, 'building_counts_all')
-
 # for pushing and pulling to s3 using the system function
 s3_bounds_prefix <- 's3://earthlab-natem/human-ignitions-wui/bounds'
 s3_anthro_prefix <- 's3://earthlab-natem/human-ignitions-wui/anthro'
@@ -61,5 +62,5 @@ s3_raw_prefix <- 's3://earthlab-natem/human-ignitions-wui/raw'
 # Check if directory exists for all variable aggregate outputs, if not then create
 var_dir <- list(prefix, summary_dir, raw_prefix, us_prefix, ecoregion_prefix, wui_prefix, fpa_prefix, mtbs_prefix, nifc_crt, ztrax_out,
                 bounds_crt, ecoreg_crt, anthro_out, fire_crt, ics_out, ics_outtbls, ics_intbls, wui_out, distance_out, bui_out, bu_out,
-                swse_crt, ics_famweb, ics_latlong, ics_spatial, ecoregion_out, fpa_out, mtbs_out, fishnet_path)
+                swse_crt, ics_famweb, ics_latlong, ics_spatial, ecoregion_out, fpa_out, mtbs_out, fishnet_path, fire_pnt, fire_poly)
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
