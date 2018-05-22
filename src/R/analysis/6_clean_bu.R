d6a5194740de254ef9da4de63c30162215507f46
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-22 22:33:40 +0000

    added states extraction

--- src/R/analysis/6_clean_bu.R
+++ src/R/analysis/6_clean_bu.R
@@ -17,7 +17,7 @@ if (!file.exists(file.path(bu_out, 'bu_masked_1990.tif'))) {
   system(paste0("aws s3 sync ", prefix, " ", s3_base))
   } else {
 
-  bu_list <- list.files(file.path(bu_out,
+  bu_list <- list.files(bu_out,
                         pattern = glob2rx('*masked*tif'),
                         full.names = TRUE)
   bu_stack <- stack(bu_list)
@@ -26,7 +26,7 @@ if (!file.exists(file.path(bu_out, 'bu_masked_1990.tif'))) {
 
 
 # Housing units per ecoregion
-if (!exists('sum_extractions_bu')) {
+if (!exists('sum_ecoregions_bu')) {
   if (!file.exists(file.path(bu_out, 'summary_ecoregion_bu.rds'))) {
 
     ecoregions <- ecoreg_plain %>%
@@ -51,6 +51,69 @@ if (!exists('sum_extractions_bu')) {
     sum_ecoregions_bu <- read_rds(file.path(bu_out, 'summary_ecoregion_bu.rds'))
   }
 } 
+
+# # Housing units per ecoregion
+# if (!exists('sum_states_bu')) {
+#   if (!file.exists(file.path(bu_out, 'summary_state_bu.rds'))) {
+#     
+#     states <- usa_shp %>%
+#       group_by(stusps) %>%
+#       summarise() %>%
+#       st_cast('POLYGON')
+#     
+#     sum_states_bu <- bu_velox$extract(sp = states, fun = function(x) sum(x, na.rm=TRUE), small = TRUE, df = TRUE) %>%
+#       as_tibble() %>%
+#       mutate(stusps = as.data.frame(states)$stusps,
+#              sum_bu_1990 = X1,
+#              sum_bu_1995 = X2,
+#              sum_bu_2000 = X3,
+#              sum_bu_2005 = X4,
+#              sum_bu_2010 = X5,
+#              sum_bu_2015 = X6) %>%
+#       dplyr::select(-starts_with('X'))
+#     write_rds(sum_states_bu, file.path(bu_out, 'summary_state_bu.rds'))
+#     system(paste0("aws s3 sync ", prefix, " ", s3_base))
+#     
+#   } else {
+#     sum_states_bu <- read_rds(file.path(bu_out, 'summary_state_bu.rds'))
+#   }
+# } 
+
+
+# Housing units per FPA fire perimeters
+if (!exists('sum_fpa_bu')) {
+  if (!file.exists(file.path(bu_out, 'summary_fpa_bu.rds'))) {
+    
+    fpa_dis <- fpa %>%
+      slice(1:10) %>%
+      mutate(year_bidecadal = ifelse(DISCOVERY_YEAR >= 1991 & DISCOVERY_YEAR <= 1995, 1995,
+                           ifelse(DISCOVERY_YEAR >= 1996 & DISCOVERY_YEAR <= 2000, 2000,
+                                  ifelse(DISCOVERY_YEAR >= 2001 & DISCOVERY_YEAR <= 2005, 2005,
+                                         ifelse(DISCOVERY_YEAR >= 2006 & DISCOVERY_YEAR <= 2010, 2010,
+                                                ifelse(DISCOVERY_YEAR >= 2011 & DISCOVERY_YEAR <= 2015, 2015,
+                                                       DISCOVERY_YEAR ))))))
+      group_by(IGNITION, year_bidecadal) %>%
+      summarise.sf()
+    
+    
+    sum_fpa_bu <- bu_velox$extract(sp = fpa, fun = function(x) sum(x, na.rm=TRUE), small = TRUE, df = TRUE) %>%
+      as_tibble() %>%
+      mutate(fpa_id = as.data.frame(fpa)$FPA_ID,
+             sum_bu_1990 = X1,
+             sum_bu_1995 = X2,
+             sum_bu_2000 = X3,
+             sum_bu_2005 = X4,
+             sum_bu_2010 = X5,
+             sum_bu_2015 = X6) %>%
+      dplyr::select(-starts_with('X'))
+    write_rds(sum_fpa_bu, file.path(bu_out, 'summary_fpa_bu.rds'))
+    system(paste0("aws s3 sync ", prefix, " ", s3_base))
+    
+  } else {
+    sum_fpa_bu <- read_rds(file.path(bu_out, 'summary_fpa_bu.rds'))
+  }
+}
+
 
 # Housing units per FPA fire perimeters
 if (!exists('sum_fpa_bu')) {
