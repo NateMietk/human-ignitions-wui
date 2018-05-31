885c1149b587a1dc2ba90ce7e907194eb5f230ca
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-31 04:13:05 +0000

    add ztrax projection

--- src/R/analysis/1_create_folders.R
+++ src/R/analysis/1_create_folders.R
@@ -9,7 +9,8 @@ source("src/functions/ggplot_theme.R")
 source("src/functions/plot_theme.R")
 
 proj_ed <- "+proj=eqdc +lat_0=39 +lon_0=-96 +lat_1=33 +lat_2=45 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs" #USA_Contiguous_Equidistant_Conic
-proj_ea <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+proj_ea <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
+proj_ztrax <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 ncores <- 5
 
 # Raw data folders
