3514ad3f2efbd442b205e9c096b7aba2cc913567
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-06-07 03:03:01 +0000

    updated

--- src/R/analysis/8b_read_data_fpa_wui.R
+++ src/R/analysis/8b_read_data_fpa_wui.R
@@ -1,6 +1,5 @@
 
 # FPA database from 1992-2015
-
 # Overall totals CLASS AND CAUSE AND FISHID25K
 fpa_wui_class_cause_fishnet25k <- fpa_wui %>%
   as.data.frame(.) %>%
@@ -13,12 +12,17 @@ totals_fpa <- fpa_wui %>%
   group_by() %>%
   summarise(totfirefreq = n(),
             totfirearea = sum(fire_size_km2),
-            totseasonlength = IQR(discovery_doy))
+            totseasonlength = IQR(discovery_doy),
+            totfire_mean = mean(fire_size_km2),
+            totfire_sd = sd(fire_size_km2)) %>%
+  mutate(totfiresize_se = totfire_sd / sqrt(totfirefreq),
+         lower_ci_totfiresize = totfire_mean - qt(1 - (0.05 / 2), totfirefreq - 1) * totfiresize_se,
+         upper_ci_totfiresize = totfire_mean + qt(1 - (0.05 / 2), totfirefreq - 1) * totfiresize_se)
 
 # Overall totals by CLASS
 totals_by_class <- fpa_wui %>%
   as.data.frame(.) %>%
-  group_by(class) %>%
+  group_by(class_coarse) %>%
   summarise(firefreq = n(),
             firearea = sum(fire_size_km2),
             seasonlength = IQR(discovery_doy),
@@ -35,7 +39,7 @@ totals_by_class_slim <- totals_by_class %>%
   mutate(totfirefreq = firefreq,
          totfirearea = firearea,
          totseasonlength = seasonlength) %>%
-  dplyr::select(class, totfirefreq, totfirearea, totseasonlength)
+  dplyr::select(class_coarse, totfirefreq, totfirearea, totseasonlength)
 
 # Overall totals by CAUSE
 totals_by_cause <- fpa_wui %>%
@@ -62,18 +66,19 @@ totals_by_cause_slim <- totals_by_cause %>%
 # Overall totals by CAUSE AND CLASS
 totals_by_cause_class <- fpa_wui %>%
   as.data.frame(.) %>%
-  group_by(class, ignition) %>%
+  group_by(class_coarse, ignition) %>%
   summarise(firefreq = n(),
             firearea = sum(fire_size_km2),
             seasonlength = IQR(discovery_doy),
             firesize_mean = mean(fire_size_km2),
             firesize_sd = sd(fire_size_km2)) %>%
+  left_join(., totals_by_class_slim, by = 'class_coarse') %>%
   mutate(firesize_se = firesize_sd / sqrt(firefreq),
          lower_ci_firesize = firesize_mean - qt(1 - (0.05 / 2), firefreq - 1) * firesize_se,
          upper_ci_firesize = firesize_mean + qt(1 - (0.05 / 2), firefreq - 1) * firesize_se,
-         pct_firefreq = (firefreq/totals_fpa$totfirefreq)*100,
-         pct_firearea = (firearea/totals_fpa$totfirearea)*100,
-         pct_allfreq = (firefreq/totals_fpa$totfirefreq)*100)
+         pct_firefreq = (firefreq/totfirefreq)*100,
+         pct_firearea = (firearea/totfirearea)*100,
+         pct_allfreq = (firefreq/totfirefreq)*100)
 
 totals_by_cause_class_slim <- totals_by_cause_class %>%
   mutate(totfirefreq = firefreq,
@@ -105,6 +110,14 @@ totals_by_cause_year <- fpa_wui %>%
          pct_firefreq = (firefreq/totfirefreq)*100,
          pct_firearea = (firearea/totfirearea)*100)
 
+totals_by_cause_year %>%
+  ggplot(aes(x = discovery_year, y = firesize_mean, color = ignition)) +
+  geom_line() +
+  geom_ribbon(data=totals_by_cause_year, 
+              aes(x = discovery_year, y = firesize_mean, 
+                  ymin = lower_ci_firesize, ymax = upper_ci_firesize, 
+                  group = ignition), 
+              alpha = 0.2) 
 
 # Overall totals by CLASS AND YEAR
 totals_by_class_year <- fpa_wui %>%
@@ -123,7 +136,14 @@ totals_by_class_year <- fpa_wui %>%
          pct_firefreq = (firefreq/totfirefreq)*100,
          pct_firearea = (firearea/totfirearea)*100)
 
-mean(totals_by_class_year$pct_firearea)
+totals_by_class_year %>%
+  ggplot(aes(x = discovery_year, y = firesize_mean, color = class)) +
+  geom_line() +
+  geom_ribbon(data = totals_by_class_year, 
+              aes(x = discovery_year, y = firesize_mean, 
+                  ymin = lower_ci_firesize, ymax = upper_ci_firesize, 
+                  group = class), 
+              alpha = 0.2) 
 
 # Overall totals by CAUSE AND CLASS AND YEAR
 totals_by_cause_class_year <- fpa_wui %>%
@@ -183,6 +203,28 @@ totals_fpa_cause_seasonal <- fpa_wui %>%
          upper_ci_firesize = firesize_mean + qt(1 - (0.05 / 2), firefreq - 1) * firesize_se,
          pct_firefreq = (firefreq/totfirefreq)*100,
          pct_firearea = (firearea/totfirearea)*100)
+
+# Overall totals by SEASON AND CAUSE and DISCOVERY YEAR
+totals_fpa_cause_seasonal_year <- fpa_wui %>%
+  as.data.frame(.) %>%
+  group_by(discovery_year, seasons, ignition) %>%
+  summarise(firefreq = n(),
+            firearea = sum(fire_size_km2),
+            seasonlength = IQR(discovery_doy),
+            firesize_mean = mean(fire_size_km2),
+            firesize_sd = sd(fire_size_km2), 
+            doy_median = median(discovery_doy)) %>%
+  mutate(firesize_se = firesize_sd / sqrt(firefreq),
+         lower_ci_firesize = firesize_mean - qt(1 - (0.05 / 2), firefreq - 1) * firesize_se,
+         upper_ci_firesize = firesize_mean + qt(1 - (0.05 / 2), firefreq - 1) * firesize_se)
+
+totals_fpa_cause_seasonal_year %>%
+  ggplot(aes(x = discovery_year, y = firesize_mean, color = ignition)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower_ci_firesize, ymax = upper_ci_firesize, 
+                  group = ignition), 
+              alpha = 0.2) +
+  facet_wrap(~ seasons)
 
 # Overall totals by CAUSE AND CLASS AND SEASON
 totals_by_cause_class_season <- fpa_wui %>%
