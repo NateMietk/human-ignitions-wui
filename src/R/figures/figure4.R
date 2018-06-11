0afde97abf68b9f79016b6b4594bcb6d77edd3e7
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-06-11 23:07:48 +0000

    update to two periods in the time series

--- src/R/figures/figure4.R
+++ src/R/figures/figure4.R
@@ -1,24 +1,19 @@
 # Distance calculations/plotting ****Fire Frequency-----------Regions --> Interaction between decadal and ignition -------------------------------
 fishdis_reg <- as.data.frame(distance_rds) %>%
-  filter(Class != 'Other' & Class != 'High Urban') %>%
-  mutate(
-    regions = ifelse(regions == 'East', 'North East', as.character(regions)),
-    distance_to_urban = distance_to_urban * 0.001,
-    decadal = ifelse(DISCOVERY_YEAR >= 1991 & DISCOVERY_YEAR <= 1995, 1995,
-                     ifelse(DISCOVERY_YEAR >= 1996 & DISCOVERY_YEAR <= 2005, 2005,
-                             ifelse(DISCOVERY_YEAR >= 2006 & DISCOVERY_YEAR <= 2015, 2015,
-                                    DISCOVERY_YEAR )))) %>%
-  group_by(fishid10k, decadal, regions, IGNITION) %>%
+  filter(class != 'Other' & class != 'High Urban') %>%
+  mutate(distance_to_urban = distance_to_urban * 0.001) %>%
+  group_by(fishid10k, ten_year, regions, ignition) %>%
   summarise(
     median_popdensity = median(pop_den),
     median_homedensity = median(house_den),
     median_distance = median(distance_to_urban),
-    fseason_lngth = IQR(DISCOVERY_DOY),
-    median_doy = median(DISCOVERY_DOY),
+    fseason_lngth = IQR(discovery_doy),
+    median_doy = median(discovery_doy),
     f_cnt = n()
   ) %>%
   ungroup() %>%
-  mutate(inter = paste0(IGNITION, "_", decadal))
+  mutate(inter = paste0(ignition, "_", ten_year)) %>%
+  filter(!is.na(ten_year))
 
 firefreq_p <- fishdis_reg %>%
   ggplot(aes(
@@ -35,7 +30,7 @@ firefreq_p <- fishdis_reg %>%
     size = 1.5
   ) +
   # scale_color_manual(values =  c('#d8b365', '#01665e')) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("Distance from urban center (km)") + ylab("Ignition frequency") +
   expand_limits(x = 0, y = 0) +
   theme_pub()  +
@@ -47,57 +42,43 @@ pred_diffs <- ggplot_build(firefreq_p)$data[[1]] %>%
   tbl_df %>%
   dplyr::select(colour, y, x, PANEL) %>%
   spread(colour, y) %>%
-  mutate(line_diff_95 = abs(`#fcae91` - `#bdd7e7`),
-         line_diff_05 = abs(`#fb6a4a` - `#6baed6`),
-         line_diff_15 = abs(`#cb181d` - `#2171b5`))
+  mutate(line_diff_1 = abs(`#fcae91` - `#bdd7e7`),
+         line_diff_2 = abs(`#fb6a4a` - `#6baed6`))
 
 min_diffs <- pred_diffs %>%
   group_by(PANEL) %>%
-  summarize(line_diff_95 = min(line_diff_95),
-            line_diff_05 = min(line_diff_05),
-            line_diff_15 = min(line_diff_15))
+  summarise(line_diff_1 = min(line_diff_1),
+            line_diff_2 = min(line_diff_2))
 
-xpoints_cnt_95 <- left_join(min_diffs, pred_diffs, by = 'line_diff_95') %>%
+xpoints_cnt_1 <- left_join(min_diffs, pred_diffs, by = 'line_diff_1') %>%
   mutate(regions = sort(unique(fishdis_reg$regions)),
          xpt_cnt = x) %>%
   dplyr::select(regions, xpt_cnt) %>%
   left_join(., fishdis_reg, by = c("regions")) %>%
   group_by(regions) %>%
   summarise(n = n(),
-            xpt_cnt_95 = round(first(xpt_cnt),0),
-            xpt_lab_95 = as.factor(xpt_cnt_95)) %>%
+            xpt_cnt_1 = round(first(xpt_cnt),0),
+            xpt_lab_1 = as.factor(xpt_cnt_1)) %>%
   ungroup()
 
-xpoints_cnt_05 <- left_join(min_diffs, pred_diffs, by = 'line_diff_05') %>%
+xpoints_cnt_2 <- left_join(min_diffs, pred_diffs, by = 'line_diff_2') %>%
   mutate(regions = sort(unique(fishdis_reg$regions)),
          xpt_cnt = x) %>%
   dplyr::select(regions, xpt_cnt) %>%
   left_join(., fishdis_reg, by = c("regions")) %>%
   group_by(regions) %>%
   summarise(n = n(),
-            xpt_cnt_05 = round(first(xpt_cnt),0),
-            xpt_lab_05 = as.factor(xpt_cnt_05)) %>%
+            xpt_cnt_2 = round(first(xpt_cnt),0),
+            xpt_lab_2 = as.factor(xpt_cnt_2)) %>%
   ungroup()
 
-xpoints_cnt_15 <- left_join(min_diffs, pred_diffs, by = 'line_diff_15') %>%
-  mutate(regions = sort(unique(fishdis_reg$regions)),
-         xpt_cnt = x) %>%
-  dplyr::select(regions, xpt_cnt) %>%
-  left_join(., fishdis_reg, by = c("regions")) %>%
-  group_by(regions) %>%
-  summarise(n = n(),
-            xpt_cnt_15 = round(first(xpt_cnt),0),
-            xpt_lab_15 = as.factor(xpt_cnt_15)) %>%
-  ungroup()
-
-xpoints_cnt <- left_join(xpoints_cnt_95, xpoints_cnt_05, by = 'regions') %>%
-  left_join(., xpoints_cnt_15, by = 'regions')
-write_csv(xpoints_cnt, "results/figs/draft/distance_firefreq_ignitiondecade.csv")
+xpoints_cnt <- left_join(xpoints_cnt_2, xpoints_cnt_1, by = c('regions', 'n')) %>%
+  write_csv(., "results/figs/draft/distance_firefreq_ignitiondecade.csv")
 
 regmean <- fishdis_reg %>%
-  group_by(regions, IGNITION) %>%
+  group_by(regions, ignition) %>%
   summarise(fcnt_mean = mean(f_cnt)) %>%
-  spread(IGNITION, fcnt_mean)
+  spread(ignition, fcnt_mean)
 
 # check to see where the min. diffs fall in plot
 firefreq_cent <- fishdis_reg %>%
@@ -114,15 +95,13 @@ firefreq_cent <- fishdis_reg %>%
     #fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("Central") +
   theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt, regions == "Central"),
+  geom_vline(aes(xintercept = xpt_cnt_1), data = subset(xpoints_cnt, regions == "Central"),
              linetype = "dashed", color  = "gray") +
-  geom_vline(aes(xintercept = xpt_cnt_05), data = subset(xpoints_cnt, regions == "Central"),
-             linetype = "dashed", color  = "gray50") +
-  geom_vline(aes(xintercept = xpt_cnt_15), data = subset(xpoints_cnt, regions == "Central"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt, regions == "Central"),
              linetype = "dashed", color  = "black") +
   theme(axis.title = element_text(face = "bold"),
         strip.text = element_text(size = 8, face = "bold"),
@@ -142,16 +121,14 @@ firefreq_west <- fishdis_reg %>%
     #fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("West") +
   theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt, regions == "West"),
-             linetype = "dashed", color  = "gray") +
-  geom_vline(aes(xintercept = xpt_cnt_05), data = subset(xpoints_cnt, regions == "West"),
-             linetype = "dashed", color  = "gray50") +
-  geom_vline(aes(xintercept = xpt_cnt_15), data = subset(xpoints_cnt, regions == "West"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt, regions == "West"),
              linetype = "dashed", color  = "black") +
+  geom_vline(aes(xintercept = xpt_cnt_1), data = subset(xpoints_cnt, regions == "West"),
+             linetype = "dashed", color  = "gray") +
   theme(axis.title = element_text(face = "bold"),
         strip.text = element_text(size = 8, face = "bold"),
         legend.position = "none")
@@ -170,11 +147,11 @@ firefreq_se <- fishdis_reg %>%
     #fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("South East") +
   theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt_95, regions == "South East"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt_2, regions == "South East"),
              linetype = "dashed", color  = "black") +
   theme(axis.title = element_text(face = "bold"),
         strip.text = element_text(size = 8, face = "bold"),
@@ -194,11 +171,11 @@ firefreq_ne <- fishdis_reg %>%
     #fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("North East") +
   theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt_95, regions == "North East"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt_2, regions == "North East"),
              linetype = "dashed", color  = "black") +
   theme(axis.title = element_text(face = "bold"),
         strip.text = element_text(size = 8, face = "bold"),
@@ -214,8 +191,8 @@ firefreq_p <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (f_cnt),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   # geom_point(alpha = 0.15) +
   geom_smooth(
@@ -232,7 +209,6 @@ firefreq_p <- fishdis_reg %>%
   facet_wrap( ~ regions, nrow = 1, scales = 'free') +
   theme(legend.position = 'none')
 
-
 pred_diffs <- ggplot_build(firefreq_p)$data[[1]] %>%
   tbl_df %>%
   dplyr::select(colour, y, x, PANEL) %>%
@@ -241,7 +217,7 @@ pred_diffs <- ggplot_build(firefreq_p)$data[[1]] %>%
 
 min_diffs <- pred_diffs %>%
   group_by(PANEL) %>%
-  summarize(line_diff = min(line_diff))
+  summarise(line_diff = min(line_diff))
 
 xpoints_cnt <- left_join(min_diffs, pred_diffs, by = 'line_diff') %>%
   mutate(regions = sort(unique(fishdis_reg$regions)),
@@ -261,8 +237,8 @@ firefreq_cent <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (f_cnt),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -285,8 +261,8 @@ firefreq_west <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (f_cnt),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -310,8 +286,8 @@ firefreq_se <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (f_cnt),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -335,8 +311,8 @@ firefreq_ne <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (f_cnt),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -365,8 +341,8 @@ firefreq_p <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (fseason_lngth),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   # geom_point(alpha = 0.15) +
   geom_smooth(
@@ -412,8 +388,8 @@ fslength_cent <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (fseason_lngth),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -436,8 +412,8 @@ fslength_west <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (fseason_lngth),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -461,8 +437,8 @@ fslength_se <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (fseason_lngth),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -486,8 +462,8 @@ fslength_ne <- fishdis_reg %>%
   ggplot(aes(
     x = (median_distance),
     y = (fseason_lngth),
-    group = IGNITION,
-    color = IGNITION
+    group = ignition,
+    color = ignition
   )) +
   geom_smooth(
     method = 'glm',
@@ -526,69 +502,55 @@ firefreq_p <- fishdis_reg %>%
     fullrange = TRUE,
     size = 1.5
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
-  xlab("Distance from urban center (km)") + ylab("Fire season length (IQR)") +
+  # scale_color_manual(values =  c('#d8b365', '#01665e')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
+  xlab("Distance from urban center (km)") + ylab("Ignition frequency") +
   expand_limits(x = 0, y = 0) +
   theme_pub()  +
-  facet_wrap( ~ regions, nrow = 1, scales = 'free') +
+  facet_wrap( ~ regions, nrow = 1, scales = 'free_x') +
   theme(legend.position = 'none')
-
 
 pred_diffs <- ggplot_build(firefreq_p)$data[[1]] %>%
   tbl_df %>%
   dplyr::select(colour, y, x, PANEL) %>%
   spread(colour, y) %>%
-  mutate(line_diff_95 = abs(`#fcae91` - `#bdd7e7`),
-         line_diff_05 = abs(`#fb6a4a` - `#6baed6`),
-         line_diff_15 = abs(`#cb181d` - `#2171b5`))
+  mutate(line_diff_1 = abs(`#fcae91` - `#bdd7e7`),
+         line_diff_2 = abs(`#fb6a4a` - `#6baed6`))
 
 min_diffs <- pred_diffs %>%
   group_by(PANEL) %>%
-  summarize(line_diff_95 = min(line_diff_95),
-            line_diff_05 = min(line_diff_05),
-            line_diff_15 = min(line_diff_15))
+  summarise(line_diff_1 = min(line_diff_1),
+            line_diff_2 = min(line_diff_2))
 
-xpoints_cnt_95 <- left_join(min_diffs, pred_diffs, by = 'line_diff_95') %>%
+xpoints_cnt_1 <- left_join(min_diffs, pred_diffs, by = 'line_diff_1') %>%
   mutate(regions = sort(unique(fishdis_reg$regions)),
          xpt_cnt = x) %>%
   dplyr::select(regions, xpt_cnt) %>%
   left_join(., fishdis_reg, by = c("regions")) %>%
   group_by(regions) %>%
   summarise(n = n(),
-            xpt_cnt_95 = round(first(xpt_cnt),0),
-            xpt_lab_95 = as.factor(xpt_cnt_95)) %>%
+            xpt_cnt_1 = round(first(xpt_cnt),0),
+            xpt_lab_1 = as.factor(xpt_cnt_1)) %>%
   ungroup()
 
-xpoints_cnt_05 <- left_join(min_diffs, pred_diffs, by = 'line_diff_05') %>%
+xpoints_cnt_2 <- left_join(min_diffs, pred_diffs, by = 'line_diff_2') %>%
   mutate(regions = sort(unique(fishdis_reg$regions)),
          xpt_cnt = x) %>%
   dplyr::select(regions, xpt_cnt) %>%
   left_join(., fishdis_reg, by = c("regions")) %>%
   group_by(regions) %>%
   summarise(n = n(),
-            xpt_cnt_05 = round(first(xpt_cnt),0),
-            xpt_lab_05 = as.factor(xpt_cnt_05)) %>%
+            xpt_cnt_2 = round(first(xpt_cnt),0),
+            xpt_lab_2 = as.factor(xpt_cnt_2)) %>%
   ungroup()
 
-xpoints_cnt_15 <- left_join(min_diffs, pred_diffs, by = 'line_diff_15') %>%
-  mutate(regions = sort(unique(fishdis_reg$regions)),
-         xpt_cnt = x) %>%
-  dplyr::select(regions, xpt_cnt) %>%
-  left_join(., fishdis_reg, by = c("regions")) %>%
-  group_by(regions) %>%
-  summarise(n = n(),
-            xpt_cnt_15 = round(first(xpt_cnt),0),
-            xpt_lab_15 = as.factor(xpt_cnt_15)) %>%
-  ungroup()
-
-xpoints_cnt <- left_join(xpoints_cnt_95, xpoints_cnt_05, by = 'regions') %>%
-  left_join(., xpoints_cnt_15, by = 'regions')
-write_csv(xpoints_cnt, "results/figs/draft/distance_fseason_ignitiondecade.csv")
+xpoints_cnt <- left_join(xpoints_cnt_2, xpoints_cnt_1, by = c('regions', 'n')) %>%
+  write_csv(., "results/figs/draft/distance_fseason_ignitiondecade.csv")
 
 regmean <- fishdis_reg %>%
-  group_by(regions, IGNITION) %>%
+  group_by(regions, ignition) %>%
   summarise(fcnt_mean = mean(f_cnt)) %>%
-  spread(IGNITION, fcnt_mean)
+  spread(ignition, fcnt_mean)
 
 # check to see where the min. diffs fall in plot
 fslength_cent <- fishdis_reg %>%
@@ -605,11 +567,11 @@ fslength_cent <- fishdis_reg %>%
     fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("Central") +
   theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt, regions == "Central"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt, regions == "Central"),
              linetype = "dashed", color  = "black") +
   theme(axis.title = element_text(face = "bold"),
         strip.text = element_text(size = 8, face = "bold"),
@@ -629,15 +591,13 @@ fslength_west <- fishdis_reg %>%
     fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("West") +
   theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt, regions == "West"),
+  geom_vline(aes(xintercept = xpt_cnt_1), data = subset(xpoints_cnt, regions == "West"),
              linetype = "dashed", color  = "gray") +
-  geom_vline(aes(xintercept = xpt_cnt_05), data = subset(xpoints_cnt, regions == "West"),
-             linetype = "dashed", color  = "gray50") +
-  geom_vline(aes(xintercept = xpt_cnt_15), data = subset(xpoints_cnt, regions == "West"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt, regions == "West"),
              linetype = "dashed", color  = "black") +
   expand_limits(x = 0, y = 0) +
   theme(axis.title = element_text(face = "bold"),
@@ -658,12 +618,12 @@ fslength_se <- fishdis_reg %>%
     fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("South East") +
-  theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt_95, regions == "South East"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt_2, regions == "North East"),
              linetype = "dashed", color  = "black") +
+  theme_pub()  +
   expand_limits(x = 0, y = 0) +
   theme(axis.title = element_text(face = "bold"),
         strip.text = element_text(size = 8, face = "bold"),
@@ -683,11 +643,11 @@ fslength_ne <- fishdis_reg %>%
     fullrange = TRUE,
     size = 1
   ) +
-  scale_color_manual(values =  c('#fcae91','#fb6a4a','#cb181d', '#bdd7e7','#6baed6','#2171b5')) +
+  scale_color_manual(values =  c('#fcae91','#fb6a4a', '#bdd7e7','#6baed6')) +
   xlab("") + ylab("") +
   ggtitle("North East") +
   theme_pub()  +
-  geom_vline(aes(xintercept = xpt_cnt_95), data = subset(xpoints_cnt_95, regions == "North East"),
+  geom_vline(aes(xintercept = xpt_cnt_2), data = subset(xpoints_cnt_2, regions == "North East"),
              linetype = "dashed", color  = "black") +
   expand_limits(x = 0, y = 0) +
   theme(axis.title = element_text(face = "bold"),
