681e4dde22f01619135c0b5c2b1406ac489aef04
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-06-15 16:00:20 +0000

    added field cleaning and csv creation

--- src/R/analysis/7_clean_bu.R
+++ src/R/analysis/7_clean_bu.R
@@ -9,24 +9,40 @@ bu_cleaned <- sum_fpa_bu %>%
   filter(fire_size_km2 > 0.00025)
 
 
-if (!file.exists(file.path(bu_out, 'bu_wuiblks.csv'))) {
+if (!file.exists(file.path(bu_out, 'bu_wui_blks.csv'))) {
   bu_wui_cleaned <- sum_wui_bu %>%
     setNames(tolower(names(.))) %>%
     gather(variable, bu, -id_sp, -blk10) %>%
     separate(variable,
              into = c("statistic", 'tmp', "year"),
              sep = "_") %>%
-    dplyr::select(-tmp) %>%
+    dplyr::select(-tmp, -id_sp, -statistic) %>%
     left_join(., as.data.frame(wui %>% setNames(tolower(names(.)))),
               by = "blk10") %>%
-    dplyr::select(-matches('(geom|s10|flag|wuiclass|veg|water|shape)')) %>%
-    filter(!(year %in% c('1995', '2005', '2015'))) 
+    dplyr::select(-matches('(geom|flag|wuiclass|veg|water|shape)')) %>%
+    filter(!(year %in% c('1995', '2005', '2015'))) %>%
+    mutate(class = ifelse(year == 1990, as.character(class90),
+                          ifelse(year == 2000, as.character(class00),
+                                 ifelse(year == 2010, as.character(class10), NA))),
+            number_of_persons = ifelse(year == 1990, pop1990,
+                                      ifelse(year == 2000, pop2000,
+                                              ifelse(year == 2010, pop2010, NA))),
+            pop_den = ifelse(year == 1990, popden1990,
+                            ifelse(year == 2000, popden2000, 
+                                    ifelse(year == 2010, popden2010, NA ))),
+            house_den = ifelse(year == 1990, huden1990,
+                              ifelse(year == 2000, huden2000,
+                                      ifelse(year == 2010, huden2010, NA ))),
+            house_units = ifelse(year == 1990, hhu1990,
+                                ifelse(year == 2000, hhu2000,
+                                        ifelse(year == 2010, hhu2010, NA )))) %>%
+          dplyr::select(blk10, year, class, bu, house_units, pop_den, house_den)
   
-  write_csv(bu_wui_cleaned, file.path(bu_out, 'bu_wuiblks.csv'))
+  write_csv(bu_wui_cleaned, file.path(bu_out, 'bu_wui_blks.csv'))
   system(paste0("aws s3 sync ", prefix, " ", s3_base))
   
 } else {
   
-  bu_wui_cleaned <- read_csv(file.path(bu_out, 'bu_wuiblks.csv'))
+  bu_wui_cleaned <- read_csv(file.path(bu_out, 'bu_wui_blks.csv'))
   
 }
