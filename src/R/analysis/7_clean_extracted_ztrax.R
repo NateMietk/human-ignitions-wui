16f69af4d1664b28c03bf06820578923b3486382
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-07-18 20:09:27 +0000

    updates

--- src/R/analysis/7_clean_extracted_ztrax.R
+++ src/R/analysis/7_clean_extracted_ztrax.R
@@ -1,4 +1,4 @@
-
+# ICS 209
 if (!file.exists(file.path(rmarkdown_files, 'bu_ics_cleaned.rds'))) {
   
   bu_ics_cleaned <- read_rds(file.path(dir_cleaned_ics_ztrax_rds, 'all_cleaned_ics_built_up.rds')) %>%
@@ -16,12 +16,20 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_ics_cleaned.rds'))) {
   bu_ics_cleaned <- read_rds(file.path(rmarkdown_files, 'bu_ics_cleaned.rds'))
 }
 
+# WUI
 if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned.rds'))) {
   
   bu_wui_cleaned <- ungroup(read_rds(file.path(dir_cleaned_wui_ztrax_rds, 'all_cleaned_wui_built_up.rds'))) %>%
     full_join(., wui_df, by = "blk10") %>%
     filter((year %in% c('1990', '2000', '2010'))) %>%
     ungroup() %>%
+    mutate(year = as.integer(year),
+           class = case_when(year == 1990 ~ as.character(class90),
+                             year == 2000 ~ as.character(class00),
+                             year == 2010 ~ as.character(class10), TRUE ~ NA_character_),
+           house_units = case_when(year == 1990 ~ house_units_1990,
+                                   year == 2000 ~ house_units_2000,
+                                   year == 2010 ~ house_units_2010, TRUE ~ NA_integer_)) %>%
     dplyr::select(blk10, year, class, built_class, build_up_count, build_up_intensity_sqm, house_units) %>%
     mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
     mutate(built_class = ifelse(is.na(built_class), 'No Structures', built_class))
@@ -33,7 +41,7 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned.rds'))) {
     filter(built_class != 'SILVIS') %>%
     mutate(built_class = 'SILVIS',
            build_up_count = as.numeric(build_up_count)) %>%
-    full_join(., bu_wui_cleaned,
+    full_join(., bu_wui_cleaned, 
               by = c('blk10', 'year', 'class', 'built_class', 'build_up_count')) %>%
     dplyr::select(-house_units) %>%
     mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
@@ -48,14 +56,17 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned.rds'))) {
   bu_wui_cleaned <- read_rds(file.path(rmarkdown_files, 'bu_wui_cleaned.rds'))
 }
 
+# FPA fire perimeters
 if (!file.exists(file.path(rmarkdown_files, 'bu_fpa_cleaned.rds'))) {
   
   bu_fpa_cleaned <- read_rds(file.path(dir_cleaned_fpa_ztrax_rds, 'all_cleaned_fpa_built_up.rds')) %>%
-    mutate(discovery_year = year) %>%
-    left_join(fpa_wui, ., by = c('discovery_year', 'fpa_id')) %>%
+    ungroup() %>%
     mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
-    mutate(built_class = ifelse(is.na(built_class), 'No Structures', built_class)) %>%
-    dplyr::select(-year)
+    mutate(discovery_year = year,
+           build_up_count_0 = build_up_count,
+           build_up_intensity_sqm_0 = build_up_intensity_sqm,
+           built_class = ifelse(is.na(built_class), 'No Structures', as.character(built_class))) %>%
+    dplyr::select(-year, -build_up_count, -build_up_intensity_sqm)
   
   write_rds(bu_fpa_cleaned, file.path(rmarkdown_files, 'bu_fpa_cleaned.rds'))
   system(paste0("aws s3 sync ", rmarkdown_files, " ", s3_rmarkdown))
@@ -65,14 +76,17 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_fpa_cleaned.rds'))) {
   bu_fpa_cleaned <- read_rds(file.path(rmarkdown_files, 'bu_fpa_cleaned.rds'))
 }
 
+# FPA 250m buffer
 if (!file.exists(file.path(rmarkdown_files, 'bu_fpa_250m_cleaned.rds'))) {
   
   bu_fpa_250m_cleaned <- read_rds(file.path(dir_cleaned_fpa_250m_ztrax_rds, 'all_cleaned_fpa_250m_built_up.rds')) %>%
-    mutate(discovery_year = year) %>%
-    left_join(fpa_wui, ., by = c('discovery_year', 'fpa_id')) %>%
+    ungroup() %>%
     mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
-    mutate(built_class = ifelse(is.na(built_class), 'No Structures', built_class)) %>%
-    dplyr::select(-year)
+    mutate(discovery_year = year,
+           build_up_count_250 = build_up_count,
+           build_up_intensity_sqm_250 = build_up_intensity_sqm,
+           built_class = ifelse(is.na(built_class), 'No Structures', as.character(built_class))) %>%
+    dplyr::select(-year, -build_up_count, -build_up_intensity_sqm)
   
   write_rds(bu_fpa_250m_cleaned, file.path(rmarkdown_files, 'bu_fpa_250m_cleaned.rds'))
   system(paste0("aws s3 sync ", rmarkdown_files, " ", s3_rmarkdown))
@@ -82,150 +96,76 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_fpa_250m_cleaned.rds'))) {
   bu_fpa_250m_cleaned <- read_rds(file.path(rmarkdown_files, 'bu_fpa_250m_cleaned.rds'))
 }
 
-as.data.frame(bu_wui_cleaned) %>%
-  # filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other', 'VLD', 'Wildlands'))) %>%
-  transform(class = factor(class, levels=c("High Urban", "Med Urban", "Low Urban",
-                                           'Interface WUI', 'Intermix WUI', 'VLD', 'Wildlands', 'Other'))) %>%
-  filter(built_class != 'Non-Residential') %>%
-  group_by(year, built_class, class) %>%
-  summarise(build_up_count = sum(build_up_count, na.rm = TRUE)) %>%
-  ggplot(aes(x = year, y = build_up_count, group = built_class, fill = built_class)) +
-  geom_bar(stat = 'identity', position=position_dodge(.7)) +
-  theme_pub() +
-  facet_wrap(~class)
+# FPA 500m buffer
+if (!file.exists(file.path(rmarkdown_files, 'bu_fpa_500m_cleaned.rds'))) {
+  
+  bu_fpa_500m_cleaned <- read_rds(file.path(dir_cleaned_fpa_500m_ztrax_rds, 'all_cleaned_fpa_500m_built_up.rds')) %>%
+    ungroup() %>%
+    mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
+    mutate(discovery_year = year,
+           build_up_count_500 = build_up_count,
+           build_up_intensity_sqm_500 = build_up_intensity_sqm,
+           built_class = ifelse(is.na(built_class), 'No Structures', as.character(built_class))) %>%
+    dplyr::select(-year, -build_up_count, -build_up_intensity_sqm)
+  
+  write_rds(bu_fpa_500m_cleaned, file.path(rmarkdown_files, 'bu_fpa_500m_cleaned.rds'))
+  system(paste0("aws s3 sync ", rmarkdown_files, " ", s3_rmarkdown))
+  
+} else {
+  
+  bu_fpa_500m_cleaned <- read_rds(file.path(rmarkdown_files, 'bu_fpa_500m_cleaned.rds'))
+}
 
-fpa_250_bu <- bu_fpa_250m_cleaned %>%
-  mutate(fpa_class = '250m buffer')
+# FPA 1000m buffer
+if (!file.exists(file.path(rmarkdown_files, 'bu_fpa_1000m_cleaned.rds'))) {
+  
+  bu_fpa_1000m_cleaned <- read_rds(file.path(dir_cleaned_fpa_1000m_ztrax_rds, 'all_cleaned_fpa_1000m_built_up.rds')) %>%
+    ungroup() %>%
+    mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
+    mutate(discovery_year = year,
+           build_up_count_1000 = build_up_count,
+           build_up_intensity_sqm_1000 = build_up_intensity_sqm,
+           built_class = ifelse(is.na(built_class), 'No Structures', as.character(built_class))) %>%
+    dplyr::select(-year, -build_up_count, -build_up_intensity_sqm)
+  
+  write_rds(bu_fpa_1000m_cleaned, file.path(rmarkdown_files, 'bu_fpa_1000m_cleaned.rds'))
+  system(paste0("aws s3 sync ", rmarkdown_files, " ", s3_rmarkdown))
+  
+} else {
+  
+  bu_fpa_1000m_cleaned <- read_rds(file.path(rmarkdown_files, 'bu_fpa_1000m_cleaned.rds'))
+}
 
-fpa_bu <- bu_fpa_cleaned %>%
-  mutate(fpa_class = '0m buffer')
-
-fpa_list <- rbind(fpa_bu, fpa_250_bu)
-
-as.data.frame(fpa_list) %>%
-  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
-  transform(class = factor(class, levels=c('Intermix WUI', 'Interface WUI', 'VLD', 'Wildlands'))) %>%
-  filter(built_class != 'No Structures') %>%
-  # filter(fpa_class != '250m buffer') %>%
-  group_by(class, fpa_class, ignition) %>%
-  summarise(build_up_count = sum(build_up_count)) %>%
-  ggplot(aes(x = class, y = build_up_count, group = fpa_class, fill = fpa_class)) +
-  geom_bar(stat = 'identity', position='stack') +
-  theme_pub() +
-  # theme(legend.position = 'none') +
-  facet_wrap(~ignition)
-
-as.data.frame(bu_fpa_cleaned) %>%
-  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
-  transform(class = factor(class, levels=c('Intermix WUI', 'Interface WUI', 'VLD', 'Wildlands'))) %>%
-  filter(built_class != 'No Structures') %>%
-  group_by(class, built_class, ignition) %>%
-  summarise(build_up_count = sum(build_up_count)) %>%
-  ggplot(aes(x = class, y = build_up_count, group = built_class, fill = built_class)) +
-  geom_bar(stat = 'identity', position='stack') +
-  theme_pub() +
-  # theme(legend.position = 'none') +
-  facet_wrap(~ignition)
-
-
-
-fpa_cause_bu_yearly <- as.data.frame(fpa_list) %>%
-  filter(fpa_class != '250m buffer') %>% 
-  #filter(fpa_class == '250m buffer') %>% 
-  group_by(ignition) %>%
-  summarise(bu_total = sum(build_up_count)) %>%
-  ungroup() %>%
-  na.omit()
-
-lt_fpa_cause_bu_yearly <- as.data.frame(fpa_list) %>%
-  filter(fpa_class != '250m buffer') %>% 
-  #filter(fpa_class == '250m buffer') %>% 
-  group_by(discovery_year, ignition) %>%
-  summarise(bu_total = sum(build_up_count)) %>%
-  ungroup() %>%
-  group_by(ignition) %>%
-  summarise(lt_mean = mean(bu_total)) %>%
-  ungroup() %>%
-  na.omit()
-
-fpa_cause_bu_df <- as.data.frame(fpa_list) %>%
-  filter(fpa_class != '250m buffer') %>% 
-  #filter(fpa_class == '250m buffer') %>% 
-  group_by(ignition, discovery_year) %>%
-  summarise(f_cnt = n(),
-            burned_area = sum(fire_size_km2),
-            bu = sum(build_up_count)) %>%
-  left_join(., fpa_cause_bu_yearly, by = 'ignition') %>%
-  left_join(., lt_fpa_cause_bu_yearly, by = 'ignition') %>%
-  mutate(pct_change = (bu - lag(bu))/lag(bu) * 100,
-         home_per_fire = bu/f_cnt,
-         home_per_areaburned = bu/burned_area,
-         anomalies = bu - lt_mean,
-         colour = ifelse(pct_change <= 0, "negative","positive"), 
-         colourlt = ifelse(anomalies <= 0, "negative","positive")) %>%
-  ungroup() %>%
-  filter(!is.na(discovery_year)) %>%
-  mutate(buff_zone = 0)
-
-fpa_cause_bu_p <- fpa_cause_bu_df %>%
-  ggplot(aes(x = discovery_year, y = bu*0.000001, group = ignition, color = ignition)) +
-  geom_point() +
-  geom_smooth(
-    method = 'glm',
-    method.args = list(family = "poisson"),
-    fullrange = TRUE,
-    size = 1, se = FALSE
-  ) +
-  theme_pub() +
-  xlab("Year") + ylab("Building unit count (in 100,000 units)") +
-  scale_color_manual(values = c("#D62728","#1F77B4")) +
-  # ggtitle('(a) Number of structures \nwithin all wildfires') +
-  #scale_y_continuous(limits = c(0, 6)) +
-  theme(axis.title = element_text(face = "bold"),
-        strip.text = element_text(size = 10, face = "bold"),
-        legend.box.background = element_rect(fill = "transparent"),
-        legend.key = element_rect(colour = "transparent", fill = "white"),
-        legend.background = element_rect(fill = "transparent"),
-        legend.title = element_blank(),
-        legend.position = 'none')
-
-doy_ignition <- as.data.frame(fpa_list) %>%
-  #filter(fpa_class != '250m buffer') %>% 
-  filter(fpa_class == '250m buffer') %>% 
-  group_by(discovery_doy, ignition) %>%
-  summarise(doy_total = sum(build_up_count)) %>%
-  ungroup()
-
-fpa_cause_bu_doy <- ggplot() +
-  geom_bar(data = filter(doy_ignition, ignition == 'Human'), aes(x =  discovery_doy, y = doy_total*0.0001, colour = ignition, fill = ignition), stat = "identity") +
-  geom_bar(data = filter(doy_ignition, ignition == 'Lightning'), aes(x =  discovery_doy, y = doy_total*0.0001, fill = ignition, colour = ignition), stat = "identity") +
-  theme_pub()  +
-  scale_color_manual(values = c("#D62728","#1F77B4")) +
-  scale_fill_manual(values = c("#D62728","#1F77B4")) +
-  xlab("Discovery day of year") + ylab("Number of homes threatened (in 1,000 units)") +
-  # ggtitle('(b) Number of structures \nwithin all wildfires') +
-  theme(axis.title = element_text(face = "bold"),
-        strip.text = element_text(size = 10, face = "bold"),
-        legend.box.background = element_rect(fill = "transparent"),
-        legend.key = element_rect(colour = "transparent", fill = "white"),
-        legend.background = element_rect(fill = "transparent"),
-        legend.title = element_blank(),
-        legend.position = 'none')
-
-fpa_cause_bu_anom <- fpa_cause_bu_df %>%
-  ggplot(aes(x =  discovery_year)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.9), aes(y = anomalies*0.000001, fill = interaction(ignition, colourlt), group = interaction(ignition, colourlt))) +
-  scale_fill_manual(values = c("#d1e5f0", '#2166ac',
-                               '#fddbc7', '#b2182b')) +
-  theme_pub() +
-  xlab("Year") + ylab("Homes threatened by wildfire anomalies (in 100,000 units)") +
-  # ggtitle('(b) Proportion of structures contained \nwithin wildfire to total') +
-  theme(axis.title = element_text(face = "bold"),
-        strip.text = element_text(size = 10, face = "bold"),
-        legend.box.background = element_rect(fill = "transparent"),
-        legend.key = element_rect(colour = "transparent", fill = "white"),
-        legend.background = element_rect(fill = "transparent"),
-        legend.title = element_blank(),
-        legend.position = 'none',
-        panel.grid.minor = element_line(colour="#f0f0f0"))
+if (!file.exists(file.path(rmarkdown_files, 'bu_complete_cleaned.rds'))) {
+  
+  fpa_wui_slim <- as.data.frame(fpa_wui) %>%
+    dplyr::select(fpa_id, discovery_year) 
+  
+  bu_complete_cleaned <- bu_fpa_1000m_cleaned %>%
+    left_join(., bu_fpa_500m_cleaned, by = c('fpa_id', 'built_class', 'discovery_year')) %>%
+    left_join(., bu_fpa_250m_cleaned, by = c('fpa_id', 'built_class', 'discovery_year')) %>%
+    left_join(., bu_fpa_cleaned, by = c('fpa_id', 'built_class', 'discovery_year'))  %>%
+    mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
+    mutate(build_up_count_250 = build_up_count_250 - build_up_count_0,
+           build_up_intensity_sqm_250 = build_up_intensity_sqm_0 - build_up_intensity_sqm_250,
+           build_up_count_500 = build_up_count_500 - (build_up_count_0 + build_up_count_250),
+           build_up_intensity_sqm_500 = build_up_intensity_sqm_500 - (build_up_intensity_sqm_0 + build_up_intensity_sqm_250),
+           build_up_count_1000 = build_up_count_1000 - (build_up_count_0 + build_up_count_250 + build_up_count_500),
+           build_up_intensity_sqm_1000 = build_up_intensity_sqm_1000 - (build_up_count_0 + build_up_count_250 + build_up_count_500)) %>%
+    left_join(fpa_wui_slim, ., by = c('discovery_year', 'fpa_id')) %>%
+    group_by(fpa_id, built_class) %>%
+    summarise_at(vars(build_up_count_0, build_up_count_250, build_up_count_500, build_up_count_1000, 
+                      build_up_intensity_sqm_0, build_up_intensity_sqm_250, build_up_intensity_sqm_500, 
+                      build_up_intensity_sqm_1000), first) %>%
+    ungroup() %>%
+    left_join(fpa_wui, ., by = 'fpa_id')
+  
+  
+  write_rds(bu_complete_cleaned, file.path(rmarkdown_files, 'bu_complete_cleaned.rds'))
+  system(paste0("aws s3 sync ", rmarkdown_files, " ", s3_rmarkdown))
+  
+} else {
+  
+  bu_complete_cleaned <- read_rds(file.path(rmarkdown_files, 'bu_complete_cleaned.rds'))
+}
 
---
+++ src/R/figures/plot_ztrax.R
@@ -0,0 +1,214 @@
+bu_complete_long <- as_tibble(as.data.frame(bu_complete_cleaned)) %>%
+  dplyr::select(fpa_id, class, ignition, discovery_year, built_class, 
+                build_up_count_0, build_up_count_250, build_up_count_500, build_up_count_1000) %>%
+  gather(key = 'buffer_class', value = 'built_count', -fpa_id, -class, -ignition, -discovery_year, -built_class) %>%
+  mutate(buffer_class = case_when(
+    buffer_class == 'build_up_count_0' ~ 'Fire perimeter',
+    buffer_class == 'build_up_count_250' ~ 'Buffer zone - 250m',
+    buffer_class == 'build_up_count_500' ~ 'Buffer zone - 500m',
+    buffer_class == 'build_up_count_1000' ~ 'Buffer zone - 1000m',
+    TRUE ~ NA_character_))
+
+
+bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  transform(buffer_class = factor(buffer_class, levels=c('Fire perimeter', 'Buffer zone - 250m', 
+                                                         'Buffer zone - 500m', 'Buffer zone - 1000m'))) %>%
+  filter(built_class != 'No Structures') %>%
+  group_by(discovery_year, buffer_class) %>%
+  summarise(built_count = sum(built_count)) %>%
+  ggplot(aes(x = discovery_year, y = built_count/1000000)) +
+  geom_bar(stat = 'identity') +
+  xlab('Discovery year') + ylab('Threatened homes (in millions)') +
+  theme_pub() +
+  theme(legend.position = 'none') +
+  facet_wrap(~ buffer_class, nrow = 1)
+#
+bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  transform(class = factor(class, levels=c('Intermix WUI', 'Interface WUI', 'VLD', 'Wildlands'))) %>%
+  transform(buffer_class = factor(buffer_class, levels=c('Fire perimeter', 'Buffer zone - 250m', 
+                                                         'Buffer zone - 500m', 'Buffer zone - 1000m'))) %>%
+  filter(built_class != 'No Structures') %>%
+  group_by(class, buffer_class) %>%
+  summarise(built_count = sum(built_count)) %>%
+  ggplot(aes(x = class, y = built_count/1000000, fill = buffer_class)) +
+  geom_bar(stat = 'identity', position='stack') +
+  scale_fill_manual(values = c("Fire perimeter" = "#D62728", 
+                               "Buffer zone - 250m" = "#FF7F0E", 
+                               "Buffer zone - 500m" =  "#2CA02C", 
+                               "Buffer zone - 1000m" = "#1F77B4"), 
+                    name="Threatened homes") +
+  xlab('Discovery year') + ylab('Threatened homes (in millions)') +
+  theme_pub() +
+  theme(legend.position = 'none')
+#
+bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  transform(class = factor(class, levels=c('Intermix WUI', 'Interface WUI', 'VLD', 'Wildlands'))) %>%
+  transform(buffer_class = factor(buffer_class, levels=c('Fire perimeter', 'Buffer zone - 250m', 
+                                                         'Buffer zone - 500m', 'Buffer zone - 1000m'))) %>%
+  filter(built_class != 'No Structures') %>%
+  group_by(ignition, buffer_class) %>%
+  summarise(built_count = sum(built_count)) %>%
+  ggplot(aes(x = ignition, y = built_count/1000000, fill = buffer_class)) +
+  geom_bar(stat = 'identity', position='stack') +
+  scale_fill_manual(values = c("Fire perimeter" = "#D62728", 
+                               "Buffer zone - 250m" = "#FF7F0E", 
+                               "Buffer zone - 500m" =  "#2CA02C", 
+                               "Buffer zone - 1000m" = "#1F77B4"), 
+                    name="Threatened homes") +
+  xlab('Discovery year') + ylab('Threatened homes (in millions)') +
+  theme_pub() +
+  theme(legend.position = 'none')
+#
+bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  transform(class = factor(class, levels=c('Intermix WUI', 'Interface WUI', 'VLD', 'Wildlands'))) %>%
+  transform(buffer_class = factor(buffer_class, levels=c('Fire perimeter', 'Buffer zone - 250m', 
+                                                         'Buffer zone - 500m', 'Buffer zone - 1000m'))) %>%
+  filter(built_class == 'Residential') %>%
+  group_by(discovery_year, buffer_class) %>%
+  summarise(built_count = sum(built_count)) %>%
+  ggplot(aes(x = discovery_year, y = built_count/1000000, fill = buffer_class)) +
+  geom_bar(stat = 'identity', position='stack') +
+  scale_fill_manual(values = c("Fire perimeter" = "#D62728", 
+                               "Buffer zone - 250m" = "#FF7F0E", 
+                               "Buffer zone - 500m" =  "#2CA02C", 
+                               "Buffer zone - 1000m" = "#1F77B4"), 
+                    name="Threatened homes") +
+  xlab('Discovery year') + ylab('Threatened homes (in millions)') +
+  theme_pub() +
+  theme(legend.position = 'none')
+#
+
+mtbs_fitted_0 <- bu_complete_long %>%
+  filter(buffer_class == 'Fire perimeter') %>%
+  filter(built_class == 'Residential') %>%
+  droplevels() %>%
+  group_by(discovery_year) %>%
+  summarise(sum_built_count = sum(built_count)/1000000) %>%
+  mblm(sum_built_count ~ discovery_year, data = ., repeated = FALSE)
+pct_increase_0 <- (max(mtbs_fitted_0$fitted.values)/min(mtbs_fitted_0$fitted.values))*100
+
+mtbs_fitted_250 <- bu_complete_long %>%
+  filter(buffer_class == "Buffer zone - 250m") %>%
+  filter(built_class == 'Residential') %>%
+  droplevels() %>%
+  group_by(discovery_year) %>%
+  summarise(sum_built_count = sum(built_count)/1000000) %>%
+  mblm(sum_built_count ~ discovery_year, data = ., repeated = FALSE)
+pct_increase_250 <- (max(mtbs_fitted_250$fitted.values)/min(mtbs_fitted_250$fitted.values))*100
+
+mtbs_fitted_500 <- bu_complete_long %>%
+  filter(buffer_class == "Buffer zone - 500m") %>%
+  filter(built_class == 'Residential') %>%
+  droplevels() %>%
+  group_by(discovery_year) %>%
+  summarise(sum_built_count = sum(built_count)/1000000) %>%
+  mblm(sum_built_count ~ discovery_year, data = ., repeated = FALSE)
+pct_increase_500 <- (max(mtbs_fitted_500$fitted.values)/min(mtbs_fitted_500$fitted.values))*100
+
+mtbs_fitted_1000 <- bu_complete_long %>%
+  filter(buffer_class == "Buffer zone - 1000m") %>%
+  filter(built_class == 'Residential') %>%
+  droplevels() %>%
+  group_by(discovery_year) %>%
+  summarise(sum_built_count = sum(built_count)/1000000) %>%
+  mblm(sum_built_count ~ discovery_year, data = ., repeated = FALSE)
+pct_increase_1000 <- (max(mtbs_fitted_1000$fitted.values)/min(mtbs_fitted_1000$fitted.values))*100
+
+bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  transform(class = factor(class, levels=c('Intermix WUI', 'Interface WUI', 'VLD', 'Wildlands'))) %>%
+  transform(buffer_class = factor(buffer_class, levels=c('Fire perimeter', 'Buffer zone - 250m', 
+                                                         'Buffer zone - 500m', 'Buffer zone - 1000m'))) %>%
+  filter(built_class == 'Residential') %>%
+  group_by(discovery_year, buffer_class) %>%
+  summarise(built_count = sum(built_count)) %>%
+  ggplot(aes(x = discovery_year, y = built_count/1000000, fill = buffer_class)) +
+  # geom_bar(stat= 'identity', position = 'stack', alpha = 0.3) +
+  scale_fill_manual(values = c("Fire perimeter" = "#D62728", 
+                               "Buffer zone - 250m" = "#FF7F0E", 
+                               "Buffer zone - 500m" =  "#2CA02C", 
+                               "Buffer zone - 1000m" = "#1F77B4"), 
+                    name="Threatened homes") +
+  geom_abline(intercept = coef(mtbs_fitted_0)[1], 
+              slope = coef(mtbs_fitted_0)[2], 
+              col = '#D62728', linetype = "dashed", size = 1.25) +
+  geom_abline(intercept = coef(mtbs_fitted_250)[1], 
+              slope = coef(mtbs_fitted_250)[2], 
+              col = '#FF7F0E', linetype = "dashed", size = 1.25) +
+  geom_abline(intercept = coef(mtbs_fitted_500)[1], 
+              slope = coef(mtbs_fitted_500)[2], 
+              col = '#2CA02C', linetype = "dashed", size = 1.25) +
+  geom_abline(intercept = coef(mtbs_fitted_1000)[1], 
+              slope = coef(mtbs_fitted_1000)[2], 
+              col = '#1F77B4', linetype = "dashed", size = 1.25) +
+  geom_text(aes(label=paste('Within perimeter % change = ', round(pct_increase_0[1],1), '%'),
+                x = 1997, y = 12), col = '#D62728', size = 4, inherit.aes = FALSE) +
+  geom_text(aes(label=paste('Within 250m buffer % change = ', round(pct_increase_250[1],1), '%'),
+                x = 1997.35, y = 11.5), col = '#FF7F0E', size = 4, inherit.aes = FALSE) +
+  geom_text(aes(label=paste('Within 500m buffer % change = ', round(pct_increase_500[1],1), '%'),
+                x = 1997.35, y = 11), col = '#2CA02C', size = 4, inherit.aes = FALSE) +
+  geom_text(aes(label=paste('Within 1000m buffer % change = ', round(pct_increase_1000[1],1), '%'),
+                x = 1997.5, y = 10.5), col = '#1F77B4', size = 4, inherit.aes = FALSE) +
+  scale_y_continuous(limits = c(0, 12)) +
+  scale_x_continuous(limits = c(1992, 2015)) +
+  xlab('Discovery year') + ylab('Threatened homes (in millions)') +
+  ggtitle('TheilSen trend estimates') +
+  theme_pub() +
+  theme(legend.position = 'none')
+
+
+class_fpa_bu_yearly <- bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  filter(built_class == 'Residential') %>%
+  droplevels() %>%
+  group_by(class) %>%
+  summarise(bu_total = sum(built_count)) %>%
+  ungroup() %>%
+  na.omit()
+
+lt_class_fpa_bu_yearly <- bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  filter(built_class == 'Residential') %>%
+  group_by(discovery_year, class) %>%
+  summarise(bu_total = sum(built_count)) %>%
+  ungroup() %>%
+  group_by(class) %>%
+  summarise(lt_mean = mean(bu_total)) %>%
+  ungroup() %>%
+  na.omit()
+
+class_fpa_bu_df <- bu_complete_long %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  filter(built_class == 'Residential') %>%
+  group_by(class, discovery_year) %>%
+  summarise(f_cnt = n(),
+            bu = sum(built_count)) %>%
+  left_join(., class_fpa_bu_yearly, by = 'class') %>%
+  left_join(., lt_class_fpa_bu_yearly, by = 'class') %>%
+  mutate(pct_change = (bu - lag(bu))/lag(bu) * 100,
+         home_per_fire = bu/f_cnt,
+         anomalies = bu - lt_mean,
+         colour = ifelse(pct_change <= 0, "negative","positive"), 
+         colourlt = ifelse(anomalies <= 0, "negative","positive")) %>%  ungroup() %>%
+  filter(!is.na(discovery_year)) 
+
+class_fpa_bu_df %>%
+  transform(class = factor(class, levels=c('Intermix WUI', 'Interface WUI', 'VLD', 'Wildlands'))) %>%
+  ggplot(aes(x =  discovery_year)) +
+  geom_bar(stat = "identity", position = "identity", aes(y = anomalies*0.000001, fill = colourlt)) +
+  scale_fill_manual(values = c(positive = "firebrick1",
+                               negative = "steelblue")) +
+  theme_pub() +
+  xlab("Year") + ylab("Homes threatened by wildfire anomalies (in 100,000 units)") +
+  theme(axis.title = element_text(face = "bold"),
+        strip.text = element_text(size = 10, face = "bold"),
+        legend.box.background = element_rect(fill = "transparent"),
+        legend.key = element_rect(colour = "transparent", fill = "white"),
+        legend.background = element_rect(fill = "transparent"),
+        legend.title = element_blank(),
+        legend.position = 'none') +
+  facet_wrap(~ class, ncol = 1, scales = 'free')
