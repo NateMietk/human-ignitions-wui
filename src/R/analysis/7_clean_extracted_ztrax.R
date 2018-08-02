1f1ff95f6dde4844c2bd4da2f26a70aea99d3b5a
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-08-02 17:41:35 +0000

    clean wui extraction/validation

--- src/R/analysis/7_clean_extracted_ztrax.R
+++ src/R/analysis/7_clean_extracted_ztrax.R
@@ -131,14 +131,14 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_ics_cleaned.rds'))) {
   
   bu_ics_cleaned <- read_rds(file.path(dir_cleaned_ics_ztrax_rds, 'all_cleaned_ics_built_up.rds')) %>%
     mutate(start_year = year) %>%
-    group_by(incident_unique_id, built_class) %>%
-    arrange(desc(incident_unique_id, built_class, year)) %>%
+    group_by(blk10, built_class) %>%
+    arrange(desc(blk10, built_class, year)) %>%
     mutate(build_up_count_no_zero = build_up_count - first(build_up_count),
            build_up_intensity_sqm_no_zero = build_up_intensity_sqm - first(build_up_intensity_sqm)) %>%
     ungroup() %>%
     left_join(wui_209, ., by = c('start_year', 'incident_unique_id')) %>%
     mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
-    mutate(built_class = ifelse(is.na(built_class), 'No Structures', as.character(built_class))) %>%
+    mutate(built_class = ifelse(is.na(built_class), 'No Structures', built_class)) %>%
     dplyr::select(-year)
   
   write_rds(bu_ics_cleaned, file.path(rmarkdown_files, 'bu_ics_cleaned.rds'))
@@ -208,9 +208,9 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned.rds'))) {
 
 # WUI all years
 if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned_validation.rds'))) {
-  # This cleaning needs a 240 GB RAM machine and takes ~24 hours
+  
   bu_wui_cleaned_validation <- ungroup(read_rds(file.path(dir_cleaned_wui_ztrax_rds, 'all_cleaned_wui_built_up.rds'))) %>%
-    group_by(blk10, built_class) %>%
+    group_by(blk10, built_class, year) %>%
     arrange(desc(blk10, built_class, year)) %>%
     mutate(build_up_count_no_zero = build_up_count - first(build_up_count),
            build_up_intensity_sqm_no_zero = build_up_intensity_sqm - first(build_up_intensity_sqm)) %>%
@@ -218,11 +218,9 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned_validation.rds'))) {
     mutate(blk10 = as.factor(blk10),
            year = as.integer(year))
   
-  wui_slim <- wui_df  %>%
-    dplyr::select(blk10, class90, class00, class10, house_units_1990, house_units_2000, house_units_2010) 
-  
-  bu_wui_cleaned_validation <- bu_wui_cleaned_validation %>%
-    left_join(., wui_slim, by = "blk10") %>%
+  bu_wui_cleaned_validation <- wui_df  %>%
+    dplyr::select(blk10, class90, class00, class10, house_units_1990, house_units_2000, house_units_2010) %>%
+    left_join(., bu_wui_cleaned_validation, by = "blk10") %>%
     mutate(class = case_when(
       year == 0 ~ as.character(class90),
       year == 1990 & year <= 1999 ~ as.character(class90),
@@ -236,10 +234,9 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned_validation.rds'))) {
     dplyr::select(blk10, year, class, built_class, build_up_count, build_up_intensity_sqm, 
                   build_up_count_no_zero, build_up_intensity_sqm_no_zero, house_units) %>%
     mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
-    group_by(blk10, built_class) %>%
-    fill(class)
+    na.omit(built_class, class)
   
-  bu_wui_cleaned_slim <- ungroup(bu_wui_cleaned_validation)  %>%
+  bu_wui_cleaned_slim <- bu_wui_cleaned_validation  %>%
     mutate(built_class = ifelse(is.na(built_class), 'No Structures', as.character(built_class))) %>%
     dplyr::select(blk10, year, class, house_units) %>%
     mutate(SILVIS = 'SILVIS') %>%
@@ -251,14 +248,11 @@ if (!file.exists(file.path(rmarkdown_files, 'bu_wui_cleaned_validation.rds'))) {
            build_up_count_no_zero = 0,
            build_up_intensity_sqm_no_zero = 0) 
   
-  bu_wui_cleaned_validation <- ungroup(bu_wui_cleaned_validation) %>%
+  bu_wui_cleaned_validation <- bu_wui_cleaned_validation %>%
     dplyr::select(-house_units) %>%
     bind_rows(list(., bu_wui_cleaned_slim)) %>%
     mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>%
-    arrange(desc(blk10, class, year, built_class)) %>%
-    mutate(blk10 = as.factor(blk10),
-           class = as.factor(class),
-           built_class = as.factor(built_class))
+    arrange(desc(blk10, class, year, built_class))
   
   write_rds(bu_wui_cleaned_validation, file.path(rmarkdown_files, 'bu_wui_cleaned_validation.rds'))
   system(paste0("aws s3 sync ", rmarkdown_files, " ", s3_rmarkdown))
