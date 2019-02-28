7ec487f49cdd9a38679e37fe11991e0e4129266a
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-02-28 22:09:19 +0000

    updated mtbs source

--- src/R/analysis/2_get_data.R
+++ src/R/analysis/2_get_data.R
@@ -71,7 +71,7 @@ if (!file.exists(fpa_gdb)) {
 
 #Download the MTBS fire polygons
 
-mtbs_shp <- file.path(mtbs_prefix, 'mtbs_perimeter_data_v2', 'dissolve_mtbs_perims_1984-2015_DD_20170501.shp')
+mtbs_shp <- file.path(mtbs_prefix, 'mtbs_perims_DD')
 if (!file.exists(mtbs_shp)) {
   loc <- "https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/MTBS_Fire/data/composite_data/burned_area_extent_shapefile/mtbs_perimeter_data.zip"
   dest <- paste0(mtbs_prefix, ".zip")
