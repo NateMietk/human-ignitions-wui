0f795fa622dc7ba6cfb28c8c20b96bcacf7a8e22
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-03-05 19:34:08 +0000

    first commit

---
+++ src/R/figures/figureS3_costs.R
@@ -0,0 +1,146 @@
+fire_sizes <- as_tibble(as.data.frame(wui_209)) %>%
+  filter(cause == 'Human') %>%
+  group_by(incident_unique_id) %>%
+  summarise(fire_size_km2 = sum(area_km2))
+
+df <- as_tibble(as.data.frame(wui_209)) %>%
+  filter(cause == 'Human') %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  transform(class = factor(class, levels=c('Interface WUI', 'Intermix WUI', 'VLD', 'Wildlands'))) %>%
+  left_join(., fire_sizes, by = 'incident_unique_id') %>%
+  mutate(fire_size_ha = fire_size_km2*100,
+         fire_size = case_when(
+           fire_size_ha >  0 & fire_size_ha < 100 ~ '0-100',
+           fire_size_ha >= 100 & fire_size_ha < 200 ~ '100-200',
+           fire_size_ha >= 200 & fire_size_ha < 400 ~ '200-400',
+           fire_size_ha >= 400 & fire_size_ha < 1000 ~ '400-1k',
+           fire_size_ha >= 1000 & fire_size_ha < 10000 ~ '1k-10k',
+           fire_size_ha >= 10000 & fire_size_ha < 50000 ~ '10k-50k',
+           fire_size_ha >= 50000 ~ '> 50k', TRUE ~ NA_character_ )) %>%
+  na.omit()
+
+aov.models <- df %>%
+  dplyr::select(class, fire_size, costs) %>%
+  filter(costs != 0) %>%
+  mutate(costs = log(costs)) %>%
+  split(.$class) %>%
+  map(~ aov(costs ~ fire_size, data = .x)) %>%
+  map( agricolae::HSD.test, trt = 'fire_size', group=TRUE) %>%
+  lapply(., '[', c(5)) %>%
+  lapply(., function(x) {
+    rownames_to_column(as.data.frame(x), var = "fire_size") %>%
+      dplyr::select(fire_size, 
+                    costs = groups.costs,
+                    groups = groups.groups)}) %>%
+  dplyr::bind_rows(., .id = 'class') 
+
+p1 <- df %>%
+  na.omit() %>%
+  transform(fire_size = factor(fire_size, levels=c('0-100', '100-200', '200-400', '400-1k', '1k-10k', '10k-50k', '> 50k'))) %>%
+  ggplot(aes(x = fire_size, y = log(costs))) +
+  geom_boxplot(position = position_dodge(width = 0.85)) +
+  scale_fill_manual(values = c("#D62728","#1F77B4")) +
+  xlab('Fire Size (ha)') + ylab('log Fire suppression costs') +
+  theme_pub() +
+  facet_wrap(~class) +
+  theme(legend.position = 'none')
+
+tt <- ggplot_build(p1)$data[[1]] %>%
+  dplyr::select(fill, group, PANEL, ymax_final) %>%
+  as_tibble() %>%
+  mutate(fire_size = case_when(
+    group == 1 ~ '0-100',
+    group == 2 ~ '100-200',
+    group == 3 ~ '200-400', 
+    group == 4 ~ '400-1k',
+    group == 5 ~ '1k-10k',
+    group == 6 ~ '10k-50k',
+    group == 7 ~ '> 50k'),
+    class = case_when(
+      PANEL == 1 ~ 'Interface WUI',
+      PANEL == 2 ~ 'Intermix WUI',
+      PANEL == 3 ~ 'VLD', 
+      PANEL == 4 ~ 'Wildlands')) %>%
+  dplyr::select(class, fire_size, ymax_final) %>%
+  left_join(aov.models, ., by = c('fire_size', 'class'))
+
+p1 <- p1 + geom_text(aes(x=fire_size, y=1.1 +ymax_final, label=groups), data=tt, fontface = "bold",
+                     position = position_dodge(width = 0.85))
+p1
+
+ggsave(file.path(main_text_figs, "figure3a_human.tiff"), p1, width = 5, height = 5, dpi = 600, scale = 3, units = "cm") #saves g
+system(paste0("aws s3 sync ", figs_dir, " ", s3_figs_dir))
+
+
+fire_sizes <- as_tibble(as.data.frame(wui_209)) %>%
+  filter(cause == 'Lightning') %>%
+  group_by(incident_unique_id) %>%
+  summarise(fire_size_km2 = sum(area_km2))
+
+df <- as_tibble(as.data.frame(wui_209)) %>%
+  filter(cause == 'Lightning') %>%
+  filter(!(class %in% c("High Urban", "Med Urban", "Low Urban", 'Other'))) %>%
+  transform(class = factor(class, levels=c('Interface WUI', 'Intermix WUI', 'VLD', 'Wildlands'))) %>%
+  left_join(., fire_sizes, by = 'incident_unique_id') %>%
+  mutate(fire_size_ha = fire_size_km2*100,
+         fire_size = case_when(
+           fire_size_ha >  0 & fire_size_ha < 100 ~ '0-100',
+           fire_size_ha >= 100 & fire_size_ha < 200 ~ '100-200',
+           fire_size_ha >= 200 & fire_size_ha < 400 ~ '200-400',
+           fire_size_ha >= 400 & fire_size_ha < 1000 ~ '400-1k',
+           fire_size_ha >= 1000 & fire_size_ha < 10000 ~ '1k-10k',
+           fire_size_ha >= 10000 & fire_size_ha < 50000 ~ '10k-50k',
+           fire_size_ha >= 50000 ~ '> 50k', TRUE ~ NA_character_ )) %>%
+  na.omit()
+
+aov.models <- df %>%
+  dplyr::select(class, fire_size, costs) %>%
+  filter(costs != 0) %>%
+  mutate(costs = log(costs)) %>%
+  split(.$class) %>%
+  map(~ aov(costs ~ fire_size, data = .x)) %>%
+  map( agricolae::HSD.test, trt = 'fire_size', group=TRUE) %>%
+  lapply(., '[', c(5)) %>%
+  lapply(., function(x) {
+    rownames_to_column(as.data.frame(x), var = "fire_size") %>%
+      dplyr::select(fire_size, 
+                    costs = groups.costs,
+                    groups = groups.groups)}) %>%
+  dplyr::bind_rows(., .id = 'class') 
+
+p1 <- df %>%
+  na.omit() %>%
+  transform(fire_size = factor(fire_size, levels=c('0-100', '100-200', '200-400', '400-1k', '1k-10k', '10k-50k', '> 50k'))) %>%
+  ggplot(aes(x = fire_size, y = log(costs))) +
+  geom_boxplot(position = position_dodge(width = 0.85)) +
+  scale_fill_manual(values = c("#D62728","#1F77B4")) +
+  xlab('Fire Size (ha)') + ylab('log Fire suppression costs') +
+  theme_pub() +
+  facet_wrap(~class) +
+  theme(legend.position = 'none')
+
+tt <- ggplot_build(p1)$data[[1]] %>%
+  dplyr::select(fill, group, PANEL, ymax_final) %>%
+  as_tibble() %>%
+  mutate(fire_size = case_when(
+    group == 1 ~ '0-100',
+    group == 2 ~ '100-200',
+    group == 3 ~ '200-400', 
+    group == 4 ~ '400-1k',
+    group == 5 ~ '1k-10k',
+    group == 6 ~ '10k-50k',
+    group == 7 ~ '> 50k'),
+    class = case_when(
+      PANEL == 1 ~ 'Interface WUI',
+      PANEL == 2 ~ 'Intermix WUI',
+      PANEL == 3 ~ 'VLD', 
+      PANEL == 4 ~ 'Wildlands')) %>%
+  dplyr::select(class, fire_size, ymax_final) %>%
+  left_join(aov.models, ., by = c('fire_size', 'class'))
+
+p1 <- p1 + geom_text(aes(x=fire_size, y=1.1 +ymax_final, label=groups), data=tt, fontface = "bold",
+                     position = position_dodge(width = 0.85))
+p1
+
+ggsave(file.path(main_text_figs, "figure3a_lightning.tiff"), p1, width = 5, height = 5, dpi = 600, scale = 3, units = "cm") #saves g
+system(paste0("aws s3 sync ", figs_dir, " ", s3_figs_dir))
