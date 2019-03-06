26e4b063eabff9c8b6f38438c3b8ab2df002a6f1
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-03-06 00:47:52 +0000

    updates to color pallette

--- src/functions/classify_pctbae.R
+++ src/functions/classify_pctbae.R
@@ -4,9 +4,11 @@ classify_pctbae <-  function(x) {
   #   - x: vector of fire sizes
   # output:
   #   - y: vector (same length) of classified fire sizes ----- Km2
-  ifelse(x < 1 & x < 10, "1 - 10",
-         ifelse(x >= 10 & x < 30, "10 - 30",
-                ifelse(x >= 30 & x < 50, "30 - 50",
-                       "> 50")))
+  ifelse(x > 0.5 & x < 10, "1 - 10",
+         ifelse(x >= 10 & x < 20, "10 - 20",
+                ifelse(x >= 20 & x < 30, "20 - 30",
+                       ifelse(x >= 30 & x < 40, "30 - 40",
+                              ifelse(x >= 40 & x < 50, "40 - 50",
+                                     "> 50")))))
   
 }
